9e8fff1f307373779811974b43233b033a1b7179
aman-barbaria <amanbarbaria@gmail.com>
2019-06-05 21:14:28 +0530

    temporary commit

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 
-const rootPrefix = '../../..',
+const rootPrefix = '../..',
   authRoutes = require(rootPrefix + '/routes/v1/auth');
 
 const router = express.Router();
