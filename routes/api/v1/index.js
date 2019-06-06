8aa6c11ac25e3c377277c829f0f32125ef2a3967
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-06-06 17:21:51 +0530

    minor.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,14 +1,40 @@
 'use strict';
 
-const express = require('express');
+const express = require('express'),
+  cookieParser = require('cookie-parser');
 
 const rootPrefix = '../../..',
-  authRoutes = require(rootPrefix + '/routes/api/v1/auth');
-// usersRoutes = require(rootPrefix + '/routes/api/v1/users');
+  authRoutes = require(rootPrefix + '/routes/api/v1/auth'),
+  basicHelper = require(rootPrefix + '/helpers/basic'),
+  coreConstant = require(rootPrefix + '/config/coreConstants'),
+  LoginCookieAuth = require(rootPrefix + '/lib/authentication/LoginCookie'),
+  apiVersions = require(rootPrefix + '/lib/globalConstant/apiVersions'),
+  userConstant = require(rootPrefix + '/lib/globalConstant/user'),
+  usersRoutes = require(rootPrefix + '/routes/api/v1/users');
+
+const errorConfig = basicHelper.fetchErrorConfig(apiVersions.v1);
+
+// Node.js cookie parsing middleware.
+router.use(cookieParser(coreConstant.COOKIE_SECRET));
+
+const validateCookie = async function(req, res, next) {
+  let loginCookieValue = req.signedCookies[userConstant.loginCookieName];
+  let authResponse = new LoginCookieAuth(loginCookieValue).perform().catch(function(r) {
+    return r;
+  });
+
+  if (authResponse.isFailure()) {
+    return authResponse.renderResponse(res, errorConfig);
+  }
+
+  req.decodedParams.currentUser = authResponse.data.currentUser;
+
+  next();
+};
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
-// router.use('/users', usersRoutes);
+router.use('/users', validateCookie, usersRoutes);
 
 module.exports = router;
