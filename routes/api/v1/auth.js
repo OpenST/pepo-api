62d2f7d2c637f5157f93cf31eaef2c0cce381e89
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-06-06 16:07:03 +0530

    minor.

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -3,32 +3,15 @@ const express = require('express'),
   cookieParser = require('cookie-parser');
 
 const rootPrefix = '../../..',
+  LoggedInUserFormatter = require(rootPrefix + '/lib/formatter/entity/LoggedInUser'),
+  routeHelper = require(rootPrefix + '/routes/helper'),
+  apiName = require(rootPrefix + '/lib/globalConstant/apiName'),
+  sanitizer = require(rootPrefix + '/helpers/sanitizer'),
   coreConstant = require(rootPrefix + '/config/coreConstants'),
   userConstant = require(rootPrefix + '/lib/globalConstant/user');
 
 // Node.js cookie parsing middleware.
 router.use(cookieParser(coreConstant.COOKIE_SECRET));
-
-router.use('/', function(req, res) {
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies);
-
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies);
-
-  console.log('cookie value', req.signedCookies.cookieName);
-
-  // var cookie = require('cookie');
-  // cookie.serialize('name', 'value');
-
-  res.status(200).json({ hello: 'world' });
-});
-
-const rootPrefix = '../../..',
-  LoggedInUserFormatter = require(rootPrefix + '/lib/formatter/entity/LoggedInUser'),
-  routeHelper = require(rootPrefix + '/routes/helper'),
-  apiName = require(rootPrefix + '/lib/globalConstant/apiName'),
-  sanitizer = require(rootPrefix + '/helpers/sanitizer');
 
 /* Create user*/
 router.get('/sign-up', sanitizer.sanitizeDynamicUrlParams, function(req, res, next) {
