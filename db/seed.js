6a7882daf03a4371d969c3b94c7d54644303454a
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-06-07 15:56:58 +0530

    fix.

--- db/seed.js
+++ db/seed.js
@@ -5,8 +5,8 @@ const rootPrefix = '..',
 const dbName = 'pepo_api_' + coreConstants.environment;
 const schemaMigrationQuery =
   'CREATE TABLE IF NOT EXISTS `schema_migrations` ' +
-  '(`version` varchar(255) COLLATE utf8mb4_general_ci NOT NULL, ' +
+  '(`version` varchar(255) COLLATE utf8_unicode_ci NOT NULL, ' +
   'PRIMARY KEY (`version`)' +
-  ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;';
+  ') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
 
 new ExecuteQuery(dbName, schemaMigrationQuery).perform();
