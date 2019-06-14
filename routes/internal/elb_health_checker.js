c187b96e5e6908a7bcb82efccb4f7ca49b8550a5
bala <bala@ost.com>
2019-06-14 21:03:28 +0530

    This is to test cloudfront caching based on Last-Modified/If-Modified-Since header. Not to panic! its a temp code.

--- routes/internal/elb_health_checker.js
+++ routes/internal/elb_health_checker.js
@@ -43,7 +43,44 @@ const decodeString = function(str) {
   return buff.toString('ascii');
 };
 
-router.get('/caching-test-1', function(req, res, next) {
+router.get('/set-cookie', function(req, res, next) {
+  const performer = function() {
+    console.log('\n\nreq.headers: ', req.headers);
+    console.log('req.cookies: ', req.cookies);
+    console.log('req.query: ', req.query);
+    console.log('req.body: ', req.body);
+
+    let dt = new Date();
+
+    let cookieOptions = {
+      maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+      httpOnly: true, // The cookie only accessible by the web server
+      // signed: true, // Indicates if the cookie should be signed
+      path: '/'
+    };
+    let val = req.query['v'] ? dt.toString() + req.query['v'] : dt.toString();
+    let etagData = encodeString(val);
+
+    // Set headers
+    res.setHeader('Date', dt);
+    res.setHeader('Last-Modified', dt);
+    console.log('ETag Val: ', val);
+    res.setHeader('ETag', etagData);
+    res.setHeader('Vary', '*');
+    res.setHeader('Cache-Control', 'max-age=50, must-revalidate');
+    res.setHeader('PepoCache', etagData);
+
+    // Set cookie
+    res.cookie('bl_ck1', etagData, cookieOptions);
+
+    // Set response
+    res.status(200).json({ setCookie: true, v: req.query['v'] });
+  };
+
+  performer();
+});
+
+router.get('/get-cookie', function(req, res, next) {
   const performer = function() {
     console.log('\n\nreq.headers: ', req.headers);
     console.log('req.cookies: ', req.cookies);
@@ -63,12 +100,6 @@ router.get('/caching-test-1', function(req, res, next) {
       headerDt = new Date(req.headers['if-modified-since']);
     }
 
-    let cookieOptions = {
-      maxAge: 1000 * 60 * 15, // would expire after 15 minutes
-      httpOnly: true, // The cookie only accessible by the web server
-      // signed: true, // Indicates if the cookie should be signed
-      path: '/'
-    };
     let val = req.query['v'] ? dt.toString() + req.query['v'] : dt.toString();
     let etagData = encodeString(val);
 
@@ -76,8 +107,6 @@ router.get('/caching-test-1', function(req, res, next) {
 
     console.log('currentMinute: ', currentMinute, ' --- headerMinute', headerMinute);
     if (currentMinute === headerMinute) {
-      res.cookie('bl_ck1', etagData + '-304', cookieOptions);
-
       res.status(304).send();
     } else {
       // Set headers
@@ -88,9 +117,6 @@ router.get('/caching-test-1', function(req, res, next) {
       res.setHeader('Vary', '*');
       res.setHeader('Cache-Control', 'max-age=50, must-revalidate');
       res.setHeader('PepoCache', etagData);
-
-      // Set cookie
-      res.cookie('bl_ck1', etagData, cookieOptions);
 
       // Set response
       res.status(200).json({ minute: currentMinute, date: dt, v: req.query['v'] });
