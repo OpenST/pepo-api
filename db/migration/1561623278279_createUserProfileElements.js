eea5274a56a489ad161101066e25cd11f3a66a02
santhu210 <santhosh.nitdgp2015@gmail.com>
2019-06-27 14:08:00 +0530

    Migration for user profile elements

---
+++ db/migration/1561623278279_createUserProfileElements.js
@@ -0,0 +1,24 @@
+const rootPrefix = '../..',
+  coreConstants = require(rootPrefix + '/config/coreConstants');
+
+const dbName = 'pepo_api_' + coreConstants.environment;
+
+const upQuery =
+  "CREATE TABLE `user_profile_elements` ( \n\
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, \n\
+  `user_id` bigint(20) unsigned NOT NULL, \n\
+  `data_kind` tinyint(4) NOT NULL, \n\
+  `data` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '', \n\
+  PRIMARY KEY (`id`), \n\
+  KEY `uk_1` (`user_id`) \n\
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+
+const downQuery = 'DROP TABLE user_profile_elements;';
+
+const migrationName = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery]
+};
+
+module.exports = migrationName;
