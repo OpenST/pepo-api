3858bee7f3c946aa5ad8ec73c8ba913730dc29e5
Tejas-Sangani <tejas@ost.com>
2019-07-09 16:30:26 +0530

    updated migration for texts table

---
+++ db/migration/1562669246298_updateTextsTable.js
@@ -0,0 +1,16 @@
+const rootPrefix = '../..',
+  coreConstants = require(rootPrefix + '/config/coreConstants');
+
+const dbName = 'pepo_api_' + coreConstants.environment;
+
+const upQuery = 'ALTER TABLE `texts` ADD COLUMN `tag_ids` varchar(255) COLLATE utf8_unicode_ci NULL AFTER `text`;';
+
+const downQuery = 'ALTER TABLE `texts` DROP `tag_ids`;';
+
+const updateTextsTable = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery]
+};
+
+module.exports = updateTextsTable;
