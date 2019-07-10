fdd819287944fe09c9e0a012ec9a8ae002c81d78
aman-barbaria <amanbarbaria@gmail.com>
2019-07-10 17:00:59 +0530

    Twitter List Sync

--- db/migration/1562665133096_create_twitter_user_connections.js
+++ db/migration/1562665133096_create_twitter_user_connections.js
@@ -11,8 +11,9 @@ const upQuery =
   `created_at` int(11) NOT NULL, \n\
   `updated_at` int(11) NOT NULL, \n\
   PRIMARY KEY (`id`), \n\
-  INDEX idx_1 (`twitter_user1_id`, `twitter_user2_id`), \n\
-  INDEX idx_2 (`twitter_user2_id`) \n\
+  UNIQUE u_idx_1 (`twitter_user1_id`, `twitter_user2_id`), \n\
+   INDEX idx_2 (`twitter_user1_id`, `properties`), \n\
+  INDEX idx_3 (`twitter_user2_id`) \n\
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
 
 const downQuery = 'drop table if exists `twitter_user_connections`;';
