c545a274ffaf43c147b0954eb157378f3b2a62fd
shlokgilda <shlok@ost.com>
2019-07-18 15:11:00 +0530

    Minor changes in user activation failure webhook processing.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- app/services/ostEvents/users/ActivationFailure.js
+++ app/services/ostEvents/users/ActivationFailure.js
@@ -1,23 +1,16 @@
 const rootPrefix = '../../../..',
-  CommonValidators = require(rootPrefix + '/lib/validators/Common'),
   TokenUserModel = require(rootPrefix + '/app/models/mysql/TokenUser'),
   UserOstEventBase = require(rootPrefix + '/app/services/ostEvents/users/Base'),
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   tokenUserConstants = require(rootPrefix + '/lib/globalConstant/tokenUser');
 
+/**
+ * Class for user activation failure webhook processor.
+ *
+ * @class UserActivationFailure
+ */
 class UserActivationFailure extends UserOstEventBase {
-  /**
-   * @param {Object} params
-   *
-   * @augments UserOstEventBase
-   *
-   * @constructor
-   */
-  constructor(params) {
-    super(params);
-  }
-
   /**
    * Async performer.
    *
@@ -39,7 +32,6 @@ class UserActivationFailure extends UserOstEventBase {
    * Validate and sanitize params.
    *
    * @return {Promise<void>}
-   *
    * @private
    */
   async _validateAndSanitizeParams() {
@@ -48,6 +40,7 @@ class UserActivationFailure extends UserOstEventBase {
     await super._validateAndSanitizeParams();
 
     if (oThis.ostUserStatus !== tokenUserConstants.createdOstStatus) {
+      // OST rolls back the status to CREATED in case of failure.
       oThis.paramErrors.push('invalid_status');
     }
 
@@ -69,7 +62,6 @@ class UserActivationFailure extends UserOstEventBase {
    * Fetch token user.
    *
    * @return {Promise<void>}
-   *
    * @private
    */
   async _fetchTokenUser() {
@@ -77,6 +69,7 @@ class UserActivationFailure extends UserOstEventBase {
 
     await super._fetchTokenUser();
 
+    // This is an extra check just to be sure that the status is definitely not activated.
     if (oThis.tokenUserObj.ostStatus === tokenUserConstants.activatedOstStatus) {
       return Promise.reject(
         responseHelper.error({
@@ -94,18 +87,17 @@ class UserActivationFailure extends UserOstEventBase {
    * Update token user status.
    *
    * @return {Promise<void>}
-   *
    * @private
    */
   async _updateTokenUser() {
     const oThis = this;
-    logger.log('Update Token User for user activation failure.');
 
+    logger.log('Updating token user for user activation failure.');
+
+    // This is an extra check to avoid updating status in case of multiple webhooks being received.
     if (oThis.tokenUserObj.ostStatus === tokenUserConstants.createdOstStatus) {
-      return Promise.resolve(responseHelper.successWithData({}));
+      return responseHelper.successWithData({});
     }
-
-    let propertyVal = oThis.tokenUserObj.properties;
 
     await new TokenUserModel()
       .update({
@@ -116,7 +108,7 @@ class UserActivationFailure extends UserOstEventBase {
 
     await TokenUserModel.flushCache({ userId: oThis.tokenUserObj.userId });
 
-    return Promise.resolve(responseHelper.successWithData({}));
+    return responseHelper.successWithData({});
   }
 }
 
