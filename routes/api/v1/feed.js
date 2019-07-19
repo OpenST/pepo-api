24397efb19690cacfebc1a27316be70cb99474c4
Dhananjay8 <dhananjay@ost.com>
2019-07-19 19:22:35 +0530

    new routes

--- routes/api/v1/feed.js
+++ routes/api/v1/feed.js
@@ -47,4 +47,36 @@ router.get('/new/:feed_id', sanitizer.sanitizeDynamicUrlParams, function(req, re
   Promise.resolve(routeHelper.perform(req, res, next, '/feed/ById', 'r_a_v1_f_1', null, dataFormatterFunc));
 });
 
+/* Content Feeds*/
+router.get('/', sanitizer.sanitizeDynamicUrlParams, function(req, res, next) {
+  req.decodedParams.apiName = apiName.userFeed;
+
+  const dataFormatterFunc = async function(serviceResponse) {
+    const wrapperFormatterRsp = await new FormatterComposer({
+      resultType: responseEntityKey.feedsList,
+      entityKindToResponseKeyMap: {
+        [entityType.feedList]: responseEntityKey.feedsList,
+        [entityType.usersMap]: responseEntityKey.users,
+        [entityType.userStats]: responseEntityKey.userStats,
+        [entityType.userProfilesMap]: responseEntityKey.userProfiles,
+        [entityType.tagsMap]: responseEntityKey.tags,
+        [entityType.linksMap]: responseEntityKey.links,
+        [entityType.imagesMap]: responseEntityKey.images,
+        [entityType.videosMap]: responseEntityKey.videos,
+        [entityType.videoDetailsMap]: responseEntityKey.videoDetails,
+        [entityType.currentUserUserContributionsMap]: responseEntityKey.currentUserUserContributions,
+        [entityType.currentUserVideoContributionsMap]: responseEntityKey.currentUserVideoContributions,
+        [entityType.feedListMeta]: responseEntityKey.meta
+      },
+      serviceData: serviceResponse.data
+    }).perform();
+
+    console.log('wrapperFormatterRsp-----', wrapperFormatterRsp);
+
+    serviceResponse.data = wrapperFormatterRsp.data;
+  };
+
+  Promise.resolve(routeHelper.perform(req, res, next, '/feed/Public', 'r_a_v1_f_2', null, dataFormatterFunc));
+});
+
 module.exports = router;
