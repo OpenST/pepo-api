c083e372339b55515f0d9197d74910776a63fa9b
Dhananjay8 <dhananjay@ost.com>
2019-07-22 13:31:39 +0530

    validate login cookie changes.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -35,24 +35,44 @@ const validateCookie = async function(req, res, next) {
 
   if (authResponse.isFailure()) {
     cookieHelper.deleteLoginCookie(res);
-    return responseHelper.renderApiResponse(authResponse, res, errorConfig);
+  } else {
+    req.decodedParams.current_user = authResponse.data.current_user;
+    req.decodedParams.user_login_cookie_value = authResponse.data.user_login_cookie_value;
   }
 
-  cookieHelper.setLoginCookie(res, authResponse.data.user_login_cookie_value);
+  // cookieHelper.setLoginCookie(res, authResponse.data.user_login_cookie_value);
 
-  req.decodedParams.current_user = authResponse.data.current_user;
+  next();
+};
+
+const validateLoginRequired = async function(req, res, next) {
+  let currentUser = req.decodedParams.current_user;
+
+  if (!currentUser) {
+    cookieHelper.deleteLoginCookie(res);
+    return responseHelper.renderApiResponse(
+      responseHelper.error({
+        internal_error_identifier: 'r_a_v1_i_1',
+        api_error_identifier: 'unauthorized_api_request'
+      }),
+      res,
+      errorConfig
+    );
+  }
+
+  cookieHelper.setLoginCookie(res, req.decodedParams.user_login_cookie_value);
 
   next();
 };
 
 router.use('/auth', authRoutes);
-router.use('/users', validateCookie, usersRoutes);
-router.use('/tokens', validateCookie, tokensRoutes);
-router.use('/ost-transactions', validateCookie, ostTransactionRoutes);
-router.use('/gifs', validateCookie, gifsRoutes);
+router.use('/users', validateCookie, validateLoginRequired, usersRoutes);
+router.use('/tokens', validateCookie, validateLoginRequired, tokensRoutes);
+router.use('/ost-transactions', validateCookie, validateLoginRequired, ostTransactionRoutes);
+router.use('/gifs', validateCookie, validateLoginRequired, gifsRoutes);
 router.use('/feeds', feedsRoutes); // TODO - temp commit - removed validateCookie for feeds. Please revert.
-router.use('/feed', validateCookie, feedRoutes);
-router.use('/activities', validateCookie, activitiesRoutes);
+router.use('/feed', validateCookie, validateLoginRequired, feedRoutes);
+router.use('/activities', validateCookie, validateLoginRequired, activitiesRoutes);
 router.use('/upload-params', uploadParamsRoutes);
 router.use('/tags', tagRoutes);
 router.use('/rotate-twitter-account', rotateTwitterAccountRoutes);
