ae50fc7ab07e801fe26c136daf2358a929824534
AnaghaMurtarkar <anagha@ost.com>
2019-07-23 15:40:00 +0530

    Ost webhooks removed.

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const responseHelper = require(rootPrefix + '/lib/formatter/response'),
   sanitizer = require(rootPrefix + '/helpers/sanitizer');
 
 const apiRoutes = require(rootPrefix + '/routes/api/index'),
-  ostWebhookRoutes = require(rootPrefix + '/routes/ostWebhook/index'),
   webhookRoutes = require(rootPrefix + '/routes/webhooks/index'),
   elbHealthCheckerRoute = require(rootPrefix + '/routes/internal/elb_health_checker');
 
@@ -156,7 +155,6 @@ app.use('/api', sanitizer.sanitizeBodyAndQuery, assignParams, apiRoutes);
 /**
  * NOTE: OST webhooks where first assign params, validate signature and then sanitize the params
  */
-app.use('/ost-webhook', ostWebhookRoutes);
 
 app.use('/webhooks', webhookRoutes);
 
