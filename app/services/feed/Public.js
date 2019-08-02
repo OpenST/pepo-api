132b37060c81c90d79204c4903af4ceb2a6eb217
Dhananjay8 <dhananjay@ost.com>
2019-08-02 11:48:15 +0530

    temp code for curated feeds

--- app/services/feed/Public.js
+++ app/services/feed/Public.js
@@ -97,19 +97,63 @@ class PublicVideoFeed extends FeedBase {
       [paginationConstants.nextPagePayloadKey]: nextPagePayloadKey
     };
 
+    // TODO - temp code for curated feeds
+    // TEMP CODE START - to show curated feeds on top(only in logged out mode)
     if (!oThis.currentUser && oThis.paginationTimestamp == null) {
       let curatedFeed = {
         id: '9999',
         primaryExternalEntityId: '999',
         paginationIdentifier: 1564468241,
         kind: 'CURATED',
-        actor: 1000,
+        actor: 9999,
         extraData: 'null',
         createdAt: 1564468241,
         updatedAt: 1564468241
       };
 
       oThis.feeds = [curatedFeed].concat(oThis.feeds);
+
+      oThis.profileResponse.userProfilesMap = Object.assign(oThis.profileResponse.userProfilesMap, {
+        '9999': {
+          id: 1000,
+          userId: 9999,
+          bio: {
+            text: 'pepo.com',
+            includes: {}
+          },
+          linkIds: [],
+          coverVideoId: '150',
+          coverImageId: '289',
+          updatedAt: 1564659388
+        }
+      });
+
+      oThis.profileResponse.usersByIdMap = Object.assign(oThis.profileResponse.usersByIdMap, {
+        '9999': {
+          id: '9999',
+          userName: 'Pepo_Academy',
+          name: 'Pepo Academy',
+          profileImageId: '292',
+          markInactiveTriggerCount: 0,
+          properties: 2,
+          status: 'ACTIVE',
+          createdAt: 1564660482,
+          updatedAt: 1564660611
+        }
+      });
+
+      oThis.profileResponse.tokenUsersByUserIdMap = Object.assign(oThis.profileResponse.tokenUsersByUserIdMap, {
+        '9999': {
+          id: '9999',
+          userId: '9999',
+          ostUserId: '20600e85-c420-47c3-97cf-5f712c80b5b6',
+          ostTokenHolderAddress: '0xc7a62816475b6d385b36318f4fb4fa90ce114b9e',
+          properties: 7,
+          ostStatus: 'ACTIVATED',
+          createdAt: 1564050302,
+          updatedAt: 1564050422
+        }
+      });
 
       oThis.profileResponse.videoMap = Object.assign(oThis.profileResponse.videoMap, {
         '999': {
@@ -133,10 +177,10 @@ class PublicVideoFeed extends FeedBase {
       oThis.profileResponse.videoDetailsMap = Object.assign(oThis.profileResponse.videoDetailsMap, {
         '999': {
           id: '999',
-          creatorUserId: '1000',
+          creatorUserId: '9999',
           videoId: '999',
-          totalContributedBy: 2,
-          totalAmount: '2122000000000000000',
+          totalContributedBy: 25,
+          totalAmount: '9000122000000000000000',
           totalTransactions: 2,
           createdAt: 1564472456,
           updatedAt: 1564472456
@@ -166,6 +210,8 @@ class PublicVideoFeed extends FeedBase {
         }
       });
     }
+
+    // TEMP CODE END - to show curated feeds on top(only in logged out mode)
 
     return responseHelper.successWithData({
       feedList: oThis.feeds,
