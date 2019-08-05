356fbf8d4f0af346884b2063b5a32dd5bb8c8beb
aman-barbaria <amanbarbaria@gmail.com>
2019-08-05 14:50:35 +0530

    cassandra models added

---
+++ app/models/cassandra/UserNotificationVisitDetail.js
@@ -0,0 +1,65 @@
+const rootPrefix = '../../..',
+  ModelBase = require(rootPrefix + '/app/models/cassandra/Base'),
+  cassandraKeyspaceConstants = require(rootPrefix + '/lib/globalConstant/cassandraKeyspace'),
+  userNotificationVisitDetailConstants = require(rootPrefix +
+    '/lib/globalConstant/cassandra/userNotificationVisitDetail');
+
+// Declare variables.
+const keyspace = cassandraKeyspaceConstants.cassandraKeyspaceName;
+
+/**
+ * Class for UserNotificationVisitDetail model.
+ *
+ * @class UserNotificationVisitDetailModel
+ */
+class UserNotificationVisitDetailModel extends ModelBase {
+  /**
+   * Constructor for activity model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ keyspace: keyspace });
+
+    const oThis = this;
+
+    oThis.tableName = 'user_notification_visit_details';
+  }
+
+  /**
+   * Format db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.entity_type
+   * @param {string} dbRow.extra_data
+   * @param {number} dbRow.status
+   * @param {number} dbRow.published_ts
+   * @param {number} dbRow.display_ts
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @return {object}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    // const formattedData = {
+    //   id: dbRow.id,
+    //   entityType: activityConstants.entityTypes[dbRow.entity_type],
+    //   entityId: dbRow.entity_id,
+    //   extraData: JSON.parse(dbRow.extra_data),
+    //   status: activityConstants.statuses[dbRow.status],
+    //   publishedTs: dbRow.published_ts,
+    //   displayTs: dbRow.display_ts,
+    //   createdAt: dbRow.created_at,
+    //   updatedAt: dbRow.updated_at
+    // };
+    //
+    // return oThis.sanitizeFormattedData(formattedData);
+  }
+}
+
+module.exports = UserNotificationVisitDetailModel;
