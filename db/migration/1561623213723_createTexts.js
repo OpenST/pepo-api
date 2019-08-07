10191125116aa2e98f19141178e6b1b38dbe0199
Dhananjay8 <dhananjay@ost.com>
2019-08-07 10:55:57 +0530

    bug fix in migrations

--- db/migration/1561623213723_createTexts.js
+++ db/migration/1561623213723_createTexts.js
@@ -6,7 +6,7 @@ const dbName = database.entityDbName;
 const upQuery =
   "CREATE TABLE `texts` ( \n\
   `id` bigint(20) NOT NULL AUTO_INCREMENT, \n\
-  `text` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' \n\
+  `text` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '', \n\
   `created_at` int(11) NOT NULL, \n\
   `updated_at` int(11) NOT NULL, \n\
   PRIMARY KEY (`id`) \n\
