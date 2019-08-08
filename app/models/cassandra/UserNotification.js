01aadb45573db57acd63cf12026cfd4e58514845
shlokgilda <shlok@ost.com>
2019-08-08 19:34:38 +0530

    Added insert and prepare query method in user notifications model.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- app/models/cassandra/UserNotification.js
+++ app/models/cassandra/UserNotification.js
@@ -1,7 +1,7 @@
 const rootPrefix = '../../..',
   ModelBase = require(rootPrefix + '/app/models/cassandra/Base'),
-  cassandraKeyspaceConstants = require(rootPrefix + '/lib/globalConstant/cassandraKeyspace'),
-  userNotificationConstants = require(rootPrefix + '/lib/globalConstant/cassandra/userNotification');
+  cassandraWrapper = require(rootPrefix + '/lib/cassandraWrapper'),
+  cassandraKeyspaceConstants = require(rootPrefix + '/lib/globalConstant/cassandraKeyspace');
 
 // Declare variables.
 const keyspace = cassandraKeyspaceConstants.cassandraKeyspaceName;
@@ -25,6 +25,7 @@ class UserNotificationModel extends ModelBase {
     const oThis = this;
 
     oThis.tableName = 'user_notifications';
+    oThis.queryTableName = `${oThis.keyspace}.${oThis.tableName}`;
   }
 
   /**
@@ -67,6 +68,95 @@ class UserNotificationModel extends ModelBase {
     };
 
     return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Insert entry in user_notifications table.
+   *
+   * @param {object} insertParameters
+   *
+   * @returns {Promise<void>}
+   */
+  async insert(insertParameters) {
+    const oThis = this;
+
+    const { queryString, valuesArray } = oThis.createInsertQueryString(insertParameters);
+
+    await cassandraWrapper.execute(queryString, valuesArray);
+  }
+
+  /**
+   * Create insert query and parameters for model.
+   *
+   * @param {object} insertParameters
+   * @param {number/string} insertParameters.user_id
+   * @param {string} insertParameters.last_action_timestamp
+   * @param {number/string} insertParameters.kind
+   * @param {number/string} insertParameters.subject_user_id
+   * @param {number/string} insertParameters.actor_count
+   * @param {number/string} insertParameters.heading_version
+   * @param {number/string} insertParameters.flag_1
+   * @param {number/string} insertParameters.flag_1
+   * @param {string} insertParameters.uuid
+   * @param {array<string/number>} insertParameters.actor_ids
+   * @param {array<string/number>} insertParameters.actor_ids
+   * @param {object} insertParameters.payload
+   * @param {object} insertParameters.column_1
+   * @param {object} insertParameters.column_2
+   *
+   * @returns {{valuesArray: [], queryString: {string}}}
+   */
+  createInsertQueryString(insertParameters) {
+    const oThis = this;
+    let queryString = `insert into ${oThis.queryTableName} (`,
+      valueString = 'values (';
+
+    const valuesArray = [];
+    let isFirstParameter = true;
+
+    for (const key in insertParameters) {
+      if (isFirstParameter) {
+        isFirstParameter = false;
+      } else {
+        queryString += ', ';
+        valueString += ', ';
+      }
+
+      switch (key) {
+        case 'user_id':
+        case 'last_action_timestamp':
+        case 'kind':
+        case 'subject_user_id':
+        case 'actor_count':
+        case 'heading_version':
+        case 'flag_1':
+        case 'flag_2':
+          valuesArray.push(Number(insertParameters[key]));
+          break;
+        case 'uuid': {
+          valuesArray.push(insertParameters[key]);
+          break;
+        }
+        case 'actor_ids': {
+          valuesArray.push(new Set(insertParameters[key]));
+          break;
+        }
+        case 'payload':
+        case 'column_1':
+        case 'column_2':
+          valuesArray.push(JSON.stringify(insertParameters[key]));
+          break;
+        default: {
+          throw new Error('Invalid key name.');
+        }
+      }
+      queryString += key;
+      valueString += '?';
+    }
+    valueString += ')';
+    queryString += `) ${valueString}`;
+
+    return { queryString, valuesArray };
   }
 
   /**
