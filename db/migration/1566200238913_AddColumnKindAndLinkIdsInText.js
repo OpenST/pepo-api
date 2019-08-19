9cf4bae53facbb34f487c2cd0a479bc9b18db2d9
Dhananjay8 <dhananjay@ost.com>
2019-08-19 20:04:43 +0530

    minor changes

--- db/migration/1566200238913_AddColumnKindAndLinkIdsInText.js
+++ db/migration/1566200238913_AddColumnKindAndLinkIdsInText.js
@@ -7,7 +7,7 @@ const dbKind = DbKindConstant.sqlDbKind;
 
 const upQuery =
   'ALTER TABLE `texts` \n\
-      ADD COLUMN `link_ids` int(11) NOT NULL AFTER `tag_ids`, \n\
+      ADD COLUMN `link_ids` int(11) AFTER `tag_ids`, \n\
       ADD COLUMN `kind` tinyint(4) NOT NULL DEFAULT 0 AFTER `link_ids`;';
 
 const downQuery = 'ALTER TABLE `texts` DROP `link_ids`, DROP `kind`;';
