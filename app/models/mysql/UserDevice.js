349c71037919e24c82e73f724d69a90b1663ce3c
Dhananjay8 <dhananjay@ost.com>
2019-08-22 16:55:43 +0530

    model for user devices.

---
+++ app/models/mysql/UserDevice.js
@@ -0,0 +1,102 @@
+const rootPrefix = '../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  userDevicesConstants = require(rootPrefix + '/lib/globalConstant/userDevice'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+
+// Declare variables.
+const dbName = databaseConstants.userDbName;
+
+/**
+ * Class for user device model.
+ *
+ * @class UserDevice
+ */
+class UserDevice extends ModelBase {
+  /**
+   * Constructor for user device model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'user_devices';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.user_id
+   * @param {number} dbRow.device_id
+   * @param {string} dbRow.token
+   * @param {string} dbRow.device_type
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @return {object}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      userId: dbRow.user_id,
+      deviceId: dbRow.device_id,
+      token: dbRow.token,
+      deviceType: userDevicesConstants.userDeviceTypes[dbRow.device_type],
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * List of formatted column names that can be exposed by service.
+   *
+   * @returns {array}
+   */
+  safeFormattedColumnNames() {
+    return ['id', 'userId', 'deviceId', 'token', 'createdAt', 'deviceType', 'updatedAt'];
+  }
+
+  /**
+   * Fetch user devices by user ids.
+   *
+   * @param {array} userIds
+   *
+   * @returns {Promise<void>}
+   */
+  async fetchByUserIds(userIds) {
+    const oThis = this;
+
+    const dbRows = await oThis
+      .select('*')
+      .where({ user_id: userIds })
+      .fire();
+
+    const response = {};
+
+    for (let index = 0; index < dbRows.length; index++) {
+      const formatDbRow = oThis.formatDbData(dbRows[index]);
+      response[formatDbRow.userId] = formatDbRow;
+    }
+
+    return response;
+  }
+
+  /**
+   * Flush cache.
+   *
+   * @returns {Promise<*>}
+   */
+  static async flushCache(params) {}
+}
+
+module.exports = UserDevice;
---
+++ db/migration/1566470193898_CreateTableUserDevices.js
@@ -0,0 +1,28 @@
+const rootPrefix = '../..',
+  database = require(rootPrefix + '/lib/globalConstant/database'),
+  DbKindConstant = require(rootPrefix + '/lib/globalConstant/dbKind');
+
+const dbName = database.userDbName;
+const dbKind = DbKindConstant.sqlDbKind;
+
+const upQuery =
+  'CREATE TABLE `user_devices` ( \n\
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,\n\
+	  `user_id` bigint(20) unsigned NOT NULL, \n\
+	  `device_id` bigint(20) unsigned NOT NULL, \n\
+	  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, \n\
+	  `created_at` int(11) NOT NULL,\n\
+    `updated_at` int(11) NOT NULL,\n\
+	  PRIMARY KEY (`id`) \n\
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
+
+const downQuery = 'drop table if exists `user_devices`;';
+
+const createUserDevicesTable = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery],
+  dbKind: dbKind
+};
+
+module.exports = createUserDevicesTable;
