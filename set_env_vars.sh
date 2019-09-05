af04caff461e4fedca2aae738e2ae110e59bf05d
shlokgilda <shlok@ost.com>
2019-09-05 16:58:38 +0530

    Bug fix.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -3,8 +3,8 @@
 export PA_ENVIRONMENT='development'
 export PA_PORT=3000
 export PA_DEBUG_ENABLED='1';
-export PA_DOMAIN='pepodev.com'
-export PA_COOKIE_DOMAIN='localhost'
+export PA_DOMAIN='www.pepo.com'
+export PA_COOKIE_DOMAIN='pepodev.com'
 
 # Devops error logs framework
 export DEVOPS_APP_NAME='pepo-api';
