726a50ac0e4b0cc0a4ec5a3b7a1ec94177b2ec92
shlokgilda <shlok@ost.com>
2019-09-12 19:16:45 +0530

    Minor bug fix.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- executables/hookProcessors/emailServiceApiCall.js
+++ executables/hookProcessors/emailServiceApiCall.js
@@ -105,7 +105,7 @@ class EmailServiceApiCall extends HookProcessorsBase {
         return SendTransactionalMail;
       }
       default: {
-        throw new Error('Unsupported event type');
+        throw new Error('Unsupported event type.');
       }
     }
   }
