ab39be3a6c181cda74a36bbb0a72dfe33cdcdcc4
Tejas-Sangani <tejas@ost.com>
2019-09-13 17:25:48 +0530

    updated migration for invite code table

--- db/migration/1568275220383_createInviteCodesTable.js
+++ db/migration/1568275220383_createInviteCodesTable.js
@@ -9,14 +9,15 @@ const upQuery =
   'CREATE TABLE `invite_codes` ( \n\
 	  `id` bigint(20) NOT NULL AUTO_INCREMENT, \n\
 	  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, \n\
-	  `invite_limit` int(11) NULL,\n\
+	  `invite_limit` int(11) NOT NULL,\n\
 	  `invited_user_count` bigint(20) NOT NULL DEFAULT 0,\n\
 	  `user_id` bigint(20) NULL,\n\
 	  `inviter_code_id` bigint(20) NULL,\n\
     `created_at` int(11) NOT NULL, \n\
     `updated_at` int(11) NOT NULL, \n\
 	  PRIMARY KEY (`id`), \n\
-	  UNIQUE KEY `idx_1` (`code`)\n\
+	  INDEX idx_1 (`inviter_code_id`),\n\
+    UNIQUE KEY `idx_2` (`code`)\n\
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
 
 const downQuery = 'drop table if exists `invite_codes`;';
