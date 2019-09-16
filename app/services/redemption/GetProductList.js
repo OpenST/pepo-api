9ba4f2e345d9e46aea5f2b35d5d3527db7d83699
Dhananjay8 <dhananjay@ost.com>
2019-09-16 18:54:09 +0530

    added price points in response

--- app/services/redemption/GetProductList.js
+++ app/services/redemption/GetProductList.js
@@ -1,6 +1,7 @@
 const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   GetUserBalance = require(rootPrefix + '/app/services/user/GetBalance'),
+  PricePointsCache = require(rootPrefix + '/lib/cacheManagement/single/PricePoints'),
   RedemptionCache = require(rootPrefix + '/lib/cacheManagement/single/RedemptionProducts'),
   RedemptionProduct = require(rootPrefix + '/app/models/mysql/redemption/Product'),
   responseHelper = require(rootPrefix + '/lib/formatter/response');
@@ -11,6 +12,7 @@ class GetRedemptionInfo extends ServiceBase {
 
     const oThis = this;
     oThis.currentUser = params.current_user;
+    oThis.pricePoints = {};
   }
 
   /**
@@ -29,12 +31,33 @@ class GetRedemptionInfo extends ServiceBase {
 
     let getUserBalanceResponse = await new GetUserBalance({ user_id: oThis.currentUser.id }).perform();
 
+    await oThis._fetchPricePoints();
+
     return Promise.resolve(
       responseHelper.successWithData({
         redemption_products: redemptionProductsRsp.data['products'],
-        balance: getUserBalanceResponse.data.balance
+        balance: getUserBalanceResponse.data.balance,
+        price_points: oThis.pricePoints
       })
     );
+  }
+
+  /**
+   * Fetch price points.
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _fetchPricePoints() {
+    const oThis = this;
+
+    const pricePointsCacheRsp = await new PricePointsCache().fetch();
+
+    if (pricePointsCacheRsp.isFailure()) {
+      return Promise.reject(pricePointsCacheRsp);
+    }
+
+    oThis.pricePoints = pricePointsCacheRsp.data;
   }
 }
 
