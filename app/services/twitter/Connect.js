34bc20d025e126b8e80f49592d261cff012eedd8
aman-barbaria <amanbarbaria@gmail.com>
2019-09-16 19:44:13 +0530

    code review change for invite flow signups

--- app/services/twitter/Connect.js
+++ app/services/twitter/Connect.js
@@ -153,10 +153,15 @@ class TwitterConnect extends ServiceBase {
       if (oThis.prelaunchInviteObj.inviterCodeId) {
         let cacheResp = await new InviteCodeByIdCache({ id: oThis.prelaunchInviteObj.inviterCodeId }).fetch();
         oThis.inviterCodeObj = cacheResp.data[oThis.prelaunchInviteObj.inviterCodeId];
+      } else {
+        await oThis._validateInviteCode();
       }
     } else {
       // Validate invite code for users, who don't have prelaunch access
-      await oThis._validateInviteCode();
+      let inviteValidationResp = await oThis._validateInviteCode();
+      if (inviteValidationResp.isFailure()) {
+        return Promise.reject(inviteValidationResp);
+      }
     }
 
     logger.log('End::Validate User signup allowed');
@@ -228,8 +233,6 @@ class TwitterConnect extends ServiceBase {
   /**
    * Fetch inviter code row from cache
    *
-   * Sets @inviterCodeObj
-   *
    * @returns {Promise<never>}
    * @private
    */
@@ -249,7 +252,7 @@ class TwitterConnect extends ServiceBase {
       );
     }
 
-    oThis.inviterCodeObj = cacheResp.data[oThis.inviteCode];
+    return cacheResp.data[oThis.inviteCode];
   }
 
   /**
@@ -265,52 +268,43 @@ class TwitterConnect extends ServiceBase {
 
     // Invite code is required but not passed
     if (!oThis.inviteCode) {
-      return Promise.reject(
-        responseHelper.paramValidationError({
-          internal_error_identifier: 's_t_c_vic_1',
-          api_error_identifier: 'invalid_api_params',
-          params_error_identifiers: ['missing_invite_code']
-        })
-      );
+      responseHelper.paramValidationError({
+        internal_error_identifier: 's_t_c_vic_1',
+        api_error_identifier: 'invalid_api_params',
+        params_error_identifiers: ['missing_invite_code']
+      });
     }
 
-    await oThis._fetchInviterCodeObject();
+    let inviterCodeRow = await oThis._fetchInviterCodeObject();
 
     // Invite code used is not present
-    if (!oThis.inviterCodeObj || !oThis.inviterCodeObj.id) {
-      return Promise.reject(
-        responseHelper.paramValidationError({
-          internal_error_identifier: 's_t_c_vic_2',
-          api_error_identifier: 'invalid_api_params',
-          params_error_identifiers: ['invalid_invite_code']
-        })
-      );
+    if (!inviterCodeRow || !inviterCodeRow.id) {
+      responseHelper.paramValidationError({
+        internal_error_identifier: 's_t_c_vic_2',
+        api_error_identifier: 'invalid_api_params',
+        params_error_identifiers: ['invalid_invite_code']
+      });
     }
 
     // If there is number of invites limit on invite code, and it has been reached
-    if (
-      oThis.inviterCodeObj.inviteLimit >= 0 &&
-      oThis.inviterCodeObj.inviteLimit <= oThis.inviterCodeObj.invitedUserCount
-    ) {
-      return Promise.reject(
-        responseHelper.paramValidationError({
-          internal_error_identifier: 's_t_c_vic_3',
-          api_error_identifier: 'invalid_api_params',
-          params_error_identifiers: ['expired_invite_code']
-        })
-      );
+    if (inviterCodeRow.inviteLimit >= 0 && inviterCodeRow.inviteLimit <= inviterCodeRow.invitedUserCount) {
+      responseHelper.paramValidationError({
+        internal_error_identifier: 's_t_c_vic_3',
+        api_error_identifier: 'invalid_api_params',
+        params_error_identifiers: ['expired_invite_code']
+      });
     }
 
     //To prevent PreLaunch Users from using his own invite code
-    if (oThis.prelaunchInviteObj && oThis.prelaunchInviteObj.inviteCodeId == oThis.inviterCodeObj.id) {
-      return Promise.reject(
-        responseHelper.paramValidationError({
-          internal_error_identifier: 's_t_c_vic_4',
-          api_error_identifier: 'invalid_api_params',
-          params_error_identifiers: ['invalid_invite_code']
-        })
-      );
+    if (oThis.prelaunchInviteObj && oThis.prelaunchInviteObj.inviteCodeId == inviterCodeRow.id) {
+      responseHelper.paramValidationError({
+        internal_error_identifier: 's_t_c_vic_4',
+        api_error_identifier: 'invalid_api_params',
+        params_error_identifiers: ['invalid_invite_code']
+      });
     }
+
+    oThis.inviterCodeObj = inviterCodeRow;
 
     return responseHelper.successWithData({});
   }
