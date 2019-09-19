10818d1759142d7f1e3468b47413ab7d8df20d29
ankitsingh <ankit@ost.com>
2019-09-19 17:16:22 +0530

    changes for request redemption.

--- config/apiParams/web/signature.js
+++ config/apiParams/web/signature.js
@@ -103,6 +103,27 @@ const v1Signature = {
       }
     ],
     optional: []
+  },
+  [apiName.requestRedemption]: {
+    mandatory: [
+      {
+        parameter: 'current_user',
+        validatorMethods: ['validateNonEmptyObject']
+      },
+      {
+        parameter: 'product_id',
+        validatorMethods: ['validateNonZeroInteger']
+      },
+      {
+        parameter: 'price_point',
+        validatorMethods: ['validateNonBlankString']
+      },
+      {
+        parameter: 'pepo_amount_in_wei',
+        validatorMethods: ['validateNonZeroWeiValue']
+      }
+    ],
+    optional: []
   }
 };
 
