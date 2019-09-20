6b80044fcca520ab2b10286405c3bddd94f69f7c
Dhananjay8 <dhananjay@ost.com>
2019-09-20 21:04:34 +0530

    code review changes

--- config/apiParams/v1/signature.js
+++ config/apiParams/v1/signature.js
@@ -824,7 +824,7 @@ const v1Signature = {
     optional: [
       {
         parameter: 'url',
-        validatorMethods: ['validateGenericUrl']
+        validatorMethods: ['validateNonBlankString']
       },
       {
         parameter: 'gotoKind',
