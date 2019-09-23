3e6009fc3fe73ee7fd3ebadd6c95aef8bc9da9da
shlokgilda <shlok@ost.com>
2019-09-23 17:04:52 +0530

    Removed unused code and moved files.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- app/services/user/notification/List.js
+++ app/services/user/notification/List.js
@@ -1,29 +1,35 @@
 const rootPrefix = '../../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
   UserModel = require(rootPrefix + '/app/models/mysql/User'),
+  CommonValidators = require(rootPrefix + '/lib/validators/Common'),
   TokenUserModel = require(rootPrefix + '/app/models/mysql/TokenUser'),
   UserMultiCache = require(rootPrefix + '/lib/cacheManagement/multi/User'),
   ImageByIdCache = require(rootPrefix + '/lib/cacheManagement/multi/ImageByIds'),
   VideoByIdCache = require(rootPrefix + '/lib/cacheManagement/multi/VideoByIds'),
+  UserNotificationModel = require(rootPrefix + '/app/models/cassandra/UserNotification'),
   NotificationResponseHelper = require(rootPrefix + '/lib/notification/response/Helper'),
   TokenUserByUserIdsMultiCache = require(rootPrefix + '/lib/cacheManagement/multi/TokenUserByUserIds'),
+  UserNotificationsByUserIdPagination = require(rootPrefix +
+    '/lib/cacheManagement/single/UserNotificationsByUserIdPagination'),
+  UpdateUserNotificationVisitDetailsService = require(rootPrefix +
+    '/app/services/user/notification/UpdateUserNotificationVisitDetails'),
   bgJob = require(rootPrefix + '/lib/rabbitMqEnqueue/bgJob'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   userConstants = require(rootPrefix + '/lib/globalConstant/user'),
   videoConstants = require(rootPrefix + '/lib/globalConstant/video'),
-  userNotificationConstants = require(rootPrefix + '/lib/globalConstant/cassandra/userNotification'),
   bgJobConstants = require(rootPrefix + '/lib/globalConstant/bgJob'),
-  UserNotificationModel = require(rootPrefix + '/app/models/cassandra/UserNotification'),
-  responseEntityKey = require(rootPrefix + '/lib/globalConstant/responseEntityKey');
+  paginationConstants = require(rootPrefix + '/lib/globalConstant/pagination'),
+  responseEntityKey = require(rootPrefix + '/lib/globalConstant/responseEntityKey'),
+  userNotificationConstants = require(rootPrefix + '/lib/globalConstant/cassandra/userNotification');
 
 /**
- * Class for user notification base.
+ * Class for user notification list.
  *
- * @class UserNotification
+ * @class UserNotificationList
  */
-class UserNotificationBase extends ServiceBase {
+class UserNotificationList extends ServiceBase {
   /**
-   * Constructor for user contribution base.
+   * Constructor for user notification list.
    *
    * @param {object} params
    * @param {object} params.current_user
@@ -37,6 +43,14 @@ class UserNotificationBase extends ServiceBase {
     super(params);
 
     const oThis = this;
+
+    oThis.currentUserId = +params.current_user.id;
+    oThis.paginationIdentifier = params[paginationConstants.paginationIdentifierKey] || null;
+
+    oThis.limit = null;
+    oThis.currentPageState = null;
+    oThis.currentPageNumber = null;
+    oThis.nextPageState = null;
 
     oThis.userNotifications = [];
     oThis.userIds = [];
@@ -64,15 +78,11 @@ class UserNotificationBase extends ServiceBase {
 
     await oThis._validateAndSanitizeParams();
 
-    await oThis._setUserNotification();
+    await oThis._setUserNotifications();
 
     await oThis._setUserAndVideoIds();
 
-    const promisesArray = [];
-
-    promisesArray.push(oThis._fetchUsers());
-    promisesArray.push(oThis._fetchTokenUsers());
-    promisesArray.push(oThis._fetchVideos());
+    const promisesArray = [oThis._fetchUsers(), oThis._fetchTokenUsers(), oThis._fetchVideos()];
     // Images are fetched later because video covers also needs to be fetched.
 
     await Promise.all(promisesArray);
@@ -92,23 +102,86 @@ class UserNotificationBase extends ServiceBase {
    * @returns {Promise<void>}
    * @private
    */
-  async _setUserNotification() {
-    throw new Error('Sub-class to implement.');
+  async _setUserNotifications() {
+    const oThis = this;
+
+    const cacheResponse = await new UserNotificationsByUserIdPagination({
+      userId: oThis.currentUserId,
+      limit: oThis.limit,
+      pageState: oThis.currentPageState,
+      pageNumber: oThis.currentPageNumber
+    }).fetch();
+    if (cacheResponse.isFailure()) {
+      return Promise.reject(cacheResponse);
+    }
+
+    oThis.userNotifications = cacheResponse.data.userNotifications;
+    oThis.nextPageState = cacheResponse.data.pageState;
   }
 
   /**
    * Validate and sanitize specific params.
    *
+   * @sets oThis.limit, oThis.currentPageState, oThis.currentPageNumber
    *
    * @returns {Promise<never>}
    * @private
    */
   async _validateAndSanitizeParams() {
-    throw new Error('Sub-class to implement.');
+    const oThis = this;
+
+    if (oThis.paginationIdentifier) {
+      const parsedPaginationParams = oThis._parsePaginationParams(oThis.paginationIdentifier);
+      oThis.currentPageState = parsedPaginationParams.page_state;
+      oThis.currentPageNumber = parsedPaginationParams.page;
+    } else {
+      oThis.currentPageState = null;
+      oThis.currentPageNumber = 1;
+    }
+
+    if (oThis.currentPageState) {
+      if (!CommonValidators.validateNonZeroInteger(oThis.currentPageNumber) || oThis.currentPageNumber < 2) {
+        return Promise.reject(
+          responseHelper.paramValidationError({
+            internal_error_identifier: 'a_s_u_n_l_vas_1',
+            api_error_identifier: 'invalid_api_params',
+            params_error_identifiers: ['invalid_pagination_identifier'],
+            debug_options: { paginationIdentifier: oThis.paginationIdentifier }
+          })
+        );
+      }
+    }
+
+    oThis.limit = paginationConstants.defaultUserNotificationPageSize;
   }
 
   /**
-   * Format notifications.
+   * Update notification centre last visit time.
+   *
+   * @returns {object}
+   * @private
+   */
+  async _updateLastVisitedTime() {
+    const oThis = this;
+
+    if (oThis.currentPageState) {
+      return;
+    }
+
+    const latestTimestamp = (oThis.formattedUserNotifications[0] || {}).lastActionTimestamp || Date.now();
+
+    const updateParam = {
+      user_id: oThis.currentUserId,
+      last_visited_at: latestTimestamp
+    };
+
+    await new UpdateUserNotificationVisitDetailsService(updateParam).perform();
+  }
+
+  /**
+   * Format notifications and update last visit time.
+   *
+   * @sets oThis.formattedUserNotifications
    *
    * @returns {Promise<never>}
    * @private
@@ -118,33 +191,30 @@ class UserNotificationBase extends ServiceBase {
 
     for (let index = 0; index < oThis.userNotifications.length; index++) {
       const userNotification = oThis.userNotifications[index];
-      const formattedUserNotification = {};
 
       if (oThis._isNotificationBlocked(userNotification)) {
         continue;
       }
-
-      formattedUserNotification.id = NotificationResponseHelper.getEncryptIdForNotification(userNotification);
-
-      formattedUserNotification.kind = userNotification.kind;
-      formattedUserNotification.timestamp = userNotification.lastActionTimestamp;
-
-      formattedUserNotification.payload = await oThis._getPayload(userNotification);
-
-      formattedUserNotification.imageId = await oThis._getImageId(userNotification);
-
+      const formattedUserNotification = {
+        id: NotificationResponseHelper.getEncryptIdForNotification(userNotification),
+        kind: userNotification.kind,
+        timestamp: userNotification.lastActionTimestamp,
+        payload: await oThis._getPayload(userNotification),
+        imageId: await oThis._getImageId(userNotification),
+        goto: await oThis._getGoto(userNotification)
+      };
       formattedUserNotification.heading = await oThis._getHeading(userNotification, formattedUserNotification.payload);
-
-      formattedUserNotification.goto = await oThis._getGoto(userNotification);
 
       oThis.formattedUserNotifications.push(formattedUserNotification);
     }
+
+    await oThis._updateLastVisitedTime();
   }
 
   /**
    * Get image id for notifications.
    *
-   * @returns {Promise<never>}
+   * @returns {Promise<*>}
    * @private
    */
   async _getImageId(userNotification) {
@@ -159,7 +229,6 @@ class UserNotificationBase extends ServiceBase {
     };
 
     const resp = NotificationResponseHelper.getImageIdForNotification(params);
-
     if (resp.isFailure()) {
       return Promise.reject(resp);
     }
@@ -170,7 +239,7 @@ class UserNotificationBase extends ServiceBase {
   /**
    * Set payload for notifications.
    *
-   * @returns {Promise<never>}
+   * @returns {Promise<*>}
    * @private
    */
   async _getPayload(userNotification) {
@@ -207,7 +276,6 @@ class UserNotificationBase extends ServiceBase {
     };
 
     const resp = NotificationResponseHelper.getHeadingForNotification(params);
-
     if (resp.isFailure()) {
       return Promise.reject(resp);
     }
@@ -216,7 +284,9 @@ class UserNotificationBase extends ServiceBase {
   }
 
   /**
-   * Set goto for notifications
+   * Set goto for notifications.
+   *
+   * @param {object} userNotification
    *
    * @returns {Promise<never>}
    * @private
@@ -249,7 +319,6 @@ class UserNotificationBase extends ServiceBase {
 
     for (let index = 0; index < oThis.userNotifications.length; index++) {
       const userNotification = oThis.userNotifications[index];
-      // const userNotification = NotificationResponseHelper.getFlattenedObject(oThis.userNotifications[index]);
 
       const supportingEntitiesConfig = NotificationResponseHelper.getSupportingEntitiesConfigForKind(
         userNotification.kind,
@@ -270,7 +339,7 @@ class UserNotificationBase extends ServiceBase {
   }
 
   /**
-   * Get  User Ids of supporting entity for a notifications
+   * Get user ids of supporting entity for a notifications.
    *
    * @returns {Promise<array>}
    * @private
@@ -296,7 +365,7 @@ class UserNotificationBase extends ServiceBase {
   }
 
   /**
-   * Get  Video Ids of supporting entity for a notifications
+   * Get video ids of supporting entity for a notifications.
    *
    * @returns {Promise<array>}
    * @private
@@ -332,18 +401,17 @@ class UserNotificationBase extends ServiceBase {
    *
    * @sets oThis.usersByIdMap, oThis.imageIds
    *
-   * @returns {Promise<void>}
+   * @returns {Promise<*>}
    * @private
    */
   async _fetchUsers() {
     const oThis = this;
 
     if (oThis.userIds.length < 1) {
-      return responseHelper.successWithData({});
+      return;
     }
 
     const usersByIdHashRes = await new UserMultiCache({ ids: oThis.userIds }).fetch();
-
     if (usersByIdHashRes.isFailure()) {
       return Promise.reject(usersByIdHashRes);
     }
@@ -356,8 +424,6 @@ class UserNotificationBase extends ServiceBase {
         oThis.imageIds.push(userObj.profileImageId);
       }
     }
-
-    return responseHelper.successWithData({});
   }
 
   /**
@@ -384,8 +450,6 @@ class UserNotificationBase extends ServiceBase {
     }
 
     oThis.tokenUsersByUserIdMap = tokenUsersByIdHashRes.data;
-
-    return responseHelper.successWithData({});
   }
 
   /**
@@ -404,7 +468,6 @@ class UserNotificationBase extends ServiceBase {
     }
 
     const cacheRsp = await new VideoByIdCache({ ids: oThis.videoIds }).fetch();
-
     if (cacheRsp.isFailure()) {
       return Promise.reject(cacheRsp);
     }
@@ -436,7 +499,6 @@ class UserNotificationBase extends ServiceBase {
     }
 
     const cacheRsp = await new ImageByIdCache({ ids: oThis.imageIds }).fetch();
-
     if (cacheRsp.isFailure()) {
       return Promise.reject(cacheRsp);
     }
@@ -445,21 +507,23 @@ class UserNotificationBase extends ServiceBase {
   }
 
   /**
-   * Check whether notification can be sent out or its blocked, due to some deleted entity
+   * Check whether notification can be sent out or its blocked, due to some deleted entity.
    *
-   * @param userNotification
+   * @param {object} userNotification
+   *
    * @returns {boolean}
    * @private
    */
   _isNotificationBlocked(userNotification) {
     const oThis = this;
 
-    if (userNotification.kind == userNotificationConstants.videoAddKind) {
+    if (userNotification.kind === userNotificationConstants.videoAddKind) {
       if (oThis.notificationVideoMap[userNotification.uuid]) {
-        for (let i = 0; i < oThis.notificationVideoMap[userNotification.uuid].length; i++) {
-          let vid = oThis.notificationVideoMap[userNotification.uuid][i];
-          if (oThis.videoMap[vid].status == videoConstants.deletedStatus) {
+        for (let index = 0; index < oThis.notificationVideoMap[userNotification.uuid].length; index++) {
+          const vid = oThis.notificationVideoMap[userNotification.uuid][index];
+          if (oThis.videoMap[vid].status === videoConstants.deletedStatus) {
             oThis.notificationsToDelete.push(userNotification);
+
             return true;
           }
         }
@@ -467,6 +531,7 @@ class UserNotificationBase extends ServiceBase {
 
       if (oThis.usersByIdMap[userNotification.actorIds[0]].status === userConstants.inActiveStatus) {
         oThis.notificationsToDelete.push(userNotification);
+
         return true;
       }
     }
@@ -475,15 +540,13 @@ class UserNotificationBase extends ServiceBase {
   }
 
   /**
-   * Enqueue background task
+   * Enqueue background task.
    *
    * @returns {Promise<void>}
    * @private
    */
   async _enqueueBackgroundTasks() {
     const oThis = this;
-
-    console.log('Notifications To Delete: ', oThis.notificationsToDelete);
 
     if (oThis.notificationsToDelete.length > 0) {
       await bgJob.enqueue(bgJobConstants.deleteCassandraJobTopic, {
@@ -513,17 +576,81 @@ class UserNotificationBase extends ServiceBase {
       tokenUserHash[userId] = new TokenUserModel().safeFormattedData(tokenUser);
     }
 
-    return {
+    const response = {
       usersByIdMap: userHash,
       tokenUsersByUserIdMap: tokenUserHash,
       imageMap: oThis.imageMap,
-      videoMap: oThis.videoMap
+      videoMap: oThis.videoMap,
+      userNotificationList: oThis.formattedUserNotifications
     };
+
+    const nextPagePayloadKey = {};
+
+    if (oThis.nextPageState) {
+      nextPagePayloadKey[paginationConstants.paginationIdentifierKey] = {
+        page_state: oThis.nextPageState,
+        page: oThis.currentPageNumber + 1
+      };
+    }
+
+    response.meta = {
+      [paginationConstants.nextPagePayloadKey]: nextPagePayloadKey
+    };
+
+    return response;
   }
 
+  /**
+   * Returns notification type.
+   *
+   * @returns {string}
+   * @private
+   */
   get _notificationType() {
     return userNotificationConstants.notificationCentreType;
   }
+
+  /**
+   * Returns default page limit.
+   *
+   * @returns {number}
+   * @private
+   */
+  _defaultPageLimit() {
+    return paginationConstants.defaultUserNotificationPageSize;
+  }
+
+  /**
+   * Returns minimum page limit.
+   *
+   * @returns {number}
+   * @private
+   */
+  _minPageLimit() {
+    return paginationConstants.defaultUserNotificationPageSize;
+  }
+
+  /**
+   * Returns maximum page limit.
+   *
+   * @returns {number}
+   * @private
+   */
+  _maxPageLimit() {
+    return paginationConstants.defaultUserNotificationPageSize;
+  }
+
+  /**
+   * Returns current page limit.
+   *
+   * @returns {number}
+   * @private
+   */
+  _currentPageLimit() {
+    const oThis = this;
+
+    return oThis.limit;
+  }
 }
 
-module.exports = UserNotificationBase;
+module.exports = UserNotificationList;
