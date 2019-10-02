cb6e907b0d0aefb3cf68e399284e736db9c0796b
shlokgilda <shlok@ost.com>
2019-10-02 19:07:20 +0530

    Added one-timer.
    Signed-off-by: shlokgilda <shlok@ost.com>

---
+++ executables/oneTimers/updateVideoAndImageStatuses.js
@@ -0,0 +1,92 @@
+const rootPrefix = '../../',
+  VideoModel = require(rootPrefix + '/app/models/mysql/Video'),
+  ImageModel = require(rootPrefix + '/app/models/mysql/Image');
+
+/**
+ * Class to update video and image statuses.
+ *
+ * @class UpdateVideoAndImageStatuses
+ */
+class UpdateVideoAndImageStatuses {
+  async perform() {
+    const oThis = this;
+
+    return oThis.asyncPerform();
+  }
+
+  async asyncPerform() {
+    const oThis = this;
+
+    const promisesArray = [oThis.updateVideosTable(), oThis.updateImagesTable()];
+
+    await Promise.all(promisesArray);
+  }
+
+  async updateVideosTable() {
+    /*
+    OLD status values:
+      {
+        '1': oThis.notCompressedStatus,
+        '2': oThis.compressionStartedStatus,
+        '3': oThis.compressionDoneStatus,
+        '4': oThis.compressionFailedStatus,
+        '5': oThis.deletedStatus
+      };
+
+    NEW compression_status values:
+      {
+        '1': oThis.notCompressedStatus,
+        '2': oThis.compressionStartedStatus,
+        '3': oThis.compressionDoneStatus,
+        '4': oThis.compressionFailedStatus
+      }
+
+    NEW status values:
+      {
+        '1': oThis.activeStatus,
+        '2': oThis.deletedStatus
+      }
+     */
+
+    await new VideoModel()
+      .update(['compression_status = status'])
+      .where(['status != 5'])
+      .fire();
+  }
+
+  async updateImagesTable() {
+    /*
+    OLD status values:
+      {
+        '1': oThis.notResized,
+        '2': oThis.resizeStarted,
+        '3': oThis.resizeDone,
+        '4': oThis.resizeFailed
+      };
+
+    NEW resize_status values:
+      {
+        '1': oThis.notResized,
+        '2': oThis.resizeStarted,
+        '3': oThis.resizeDone,
+        '4': oThis.resizeFailed
+      }
+
+    NEW status values:
+      { '1': oThis.activeStatus }
+     */
+
+    await new ImageModel().update(['resize_status = status']).fire();
+  }
+}
+
+new UpdateVideoAndImageStatuses()
+  .perform()
+  .then((response) => {
+    console.log(response);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
