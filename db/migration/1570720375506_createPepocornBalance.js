c78eaf94baf7dcfeccb289b9f780662ee41733ad
shlokgilda <shlok@ost.com>
2019-10-14 15:06:08 +0530

    Added unique index in pepocorn_balances.
    Signed-off-by: shlokgilda <shlok@ost.com>

--- db/migration/1570720375506_createPepocornBalance.js
+++ db/migration/1570720375506_createPepocornBalance.js
@@ -1,9 +1,9 @@
 const rootPrefix = '../..',
-  database = require(rootPrefix + '/lib/globalConstant/database'),
-  DbKindConstant = require(rootPrefix + '/lib/globalConstant/dbKind');
+  dbKindConstants = require(rootPrefix + '/lib/globalConstant/dbKind'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
 
-const dbName = database.redemptionDbName;
-const dbKind = DbKindConstant.sqlDbKind;
+const dbName = databaseConstants.redemptionDbName;
+const dbKind = dbKindConstants.sqlDbKind;
 
 const upQuery =
   'CREATE TABLE `pepocorn_balances` ( \n\
@@ -12,7 +12,8 @@ const upQuery =
   `balance` int(11) NOT NULL  DEFAULT 0, \n\
   `created_at` int(11) NOT NULL, \n\
   `updated_at` int(11) NOT NULL, \n\
-  PRIMARY KEY (`id`) \n\
+  PRIMARY KEY (`id`), \n\
+  UNIQUE KEY un_idx_1 (`user_id`) \n\
 ) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
 
 const downQuery = 'drop table if exists `pepocorn_balances`;';
