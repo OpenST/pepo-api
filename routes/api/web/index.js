b65aacaf77522a09eba0c15cff389bb29ad6df4b
Kedar Chandrayan <kedarchandrayan@gmail.com>
2019-10-17 19:29:44 +0530

    clean up

--- routes/api/web/index.js
+++ routes/api/web/index.js
@@ -4,10 +4,8 @@ const express = require('express'),
 
 const rootPrefix = '../../..',
   preLaunchRoutes = require(rootPrefix + '/routes/api/web/preLaunch'),
-  redemptionsRoutes = require(rootPrefix + '/routes/api/web/redemptions'),
   coreConstants = require(rootPrefix + '/config/coreConstants'),
-  supportRoutes = require(rootPrefix + '/routes/api/web/support'),
-  cookieHelper = require(rootPrefix + '/lib/cookieHelper');
+  supportRoutes = require(rootPrefix + '/routes/api/web/support');
 
 // Node.js cookie parsing middleware.
 router.use(cookieParser(coreConstants.WEB_COOKIE_SECRET));
@@ -16,8 +14,6 @@ router.use(cookieParser(coreConstants.WEB_COOKIE_SECRET));
 // router.use(cookieHelper.setWebCsrf());
 
 router.use('/prelaunch', preLaunchRoutes);
-
-router.use('/redemptions', redemptionsRoutes);
 
 router.use('/support', supportRoutes);
 
