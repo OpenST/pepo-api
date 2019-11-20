175577e2fd3d0db7fe4f29715d1c6a516d423ff8
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2019-11-20 17:07:50 +0530

    Flush cache issue in delete reply video.

--- app/models/mysql/ReplyDetail.js
+++ app/models/mysql/ReplyDetail.js
@@ -365,6 +365,7 @@ class ReplyDetail extends ModelBase {
    * Flush cache.
    *
    * @param {object} params
+   * @param {number} [params.userId]
    * @param {array<number>} [params.parentVideoIds]
    * @param {number} [params.replyDetailId]
    * @param {array<number>} [params.replyDetailIds]
@@ -375,6 +376,12 @@ class ReplyDetail extends ModelBase {
    */
   static async flushCache(params) {
     const promisesArray = [];
+
+    if (params.userId) {
+      const ReplyDetailsByUserIdPaginationCache = require(rootPrefix +
+        '/lib/cacheManagement/single/ReplyDetailsByUserIdPagination');
+      promisesArray.push(new ReplyDetailsByUserIdPaginationCache({ userId: params.userId }).clear());
+    }
 
     if (params.parentVideoIds) {
       const ReplyDetailsByParentVideoPaginationCache = require(rootPrefix +
