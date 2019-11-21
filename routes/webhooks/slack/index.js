02b9378c972ce6ed822cdb7dddb06982d5b9542d
Tejas-Sangani <tejas@ost.com>
2019-11-21 17:21:08 +0530

    change in slack route for staging testing

--- routes/webhooks/slack/index.js
+++ routes/webhooks/slack/index.js
@@ -16,7 +16,7 @@ const validateSlackSignature = async function(req, res, next) {
 
   authResponse = await new SlackWebhookAuth({
     rawBody: req.rawBody,
-    webhookParams: req.body,
+    webhookParams: req.decodedParams.webhookParams,
     requestHeaders: req.headers
   })
     .perform()
@@ -30,6 +30,18 @@ const validateSlackSignature = async function(req, res, next) {
 
   req.decodedParams.current_admin = authResponse.data.current_admin;
 
+  next();
+};
+
+/**
+ * Convert String to Json
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+const formatPayload = function(req, res, next) {
+  req.body.payload = JSON.parse(req.body.payload);
   next();
 };
 
@@ -50,16 +62,49 @@ const assignParams = function(req, res, next) {
   next();
 };
 
-router.use(validateSlackSignature, sanitizer.sanitizeBodyAndQuery, assignParams);
+router.use(formatPayload, sanitizer.sanitizeBodyAndQuery, assignParams, validateSlackSignature);
 
-/* Listen to Ost Events*/
+/* Listen to Slack Events*/
 router.post('/', sanitizer.sanitizeDynamicUrlParams, function(req, res, next) {
   const performer = async function() {
-    let response = await new SlackEventFactory(req.decodedParams).perform();
+    let response = responseHelper.successWithData({});
+    // let response = responseHelper.error({
+    //   internal_error_identifier: 'r_a_w_pl_7',
+    //   api_error_identifier: 'resource_not_found',
+    //   debug_options: {}
+    // });
+
     return responseHelper.renderApiResponse(response, res, errorConfig);
+
+    if (response.success) {
+      return res.status(200).json({});
+    } else {
+      let status = response._fetchHttpCode(errorConfig.api_error_config || {});
+      let errorResponse = response.toHash(errorConfig);
+
+      let formattedResponse = {
+        response_type: 'ephermal',
+        replace_original: false,
+        text: 'DUMMY ERROR'
+      };
+
+      return res.status(status).json(formattedResponse);
+    }
+
+    // let response = await new SlackEventFactory(req.decodedParams).perform();
   };
 
   performer();
 });
+
+// /* Listen to Slack Events*/
+// router.post('/', sanitizer.sanitizeDynamicUrlParams, function(req, res, next) {
+//   const performer = async function() {
+//     let response = await new SlackEventFactory(req.decodedParams).perform();
+//     return responseHelper.renderApiResponse(response, res, errorConfig);
+//   };
+//
+//   performer();
+// });
 
 module.exports = router;
