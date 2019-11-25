e6ed66e0bac3f90cd2f26d024d871cd23e2430c1
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2019-11-25 16:33:40 +0530

    Validations added.

--- app/services/video/Validate.js
+++ app/services/video/Validate.js
@@ -1,5 +1,7 @@
 const rootPrefix = '../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base'),
+  CommonValidators = require(rootPrefix + '/lib/validators/Common'),
+  basicHelper = require(rootPrefix + '/helpers/basic'),
   responseHelper = require(rootPrefix + '/lib/formatter/response');
 
 /**
@@ -39,7 +41,33 @@ class ValidateUploadVideoParams extends ServiceBase {
    * @private
    */
   async _asyncPerform() {
+    const oThis = this;
+
+    await oThis._validate();
     return responseHelper.successWithData({});
+  }
+
+  /**
+   * Validate.
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _validate() {
+    const oThis = this;
+
+    const perReplyAmount = basicHelper.convertWeiToNormal(oThis.perReplyAmountInWei);
+
+    if (!CommonValidators.validateInteger(perReplyAmount.toString(10))) {
+      return Promise.reject(
+        responseHelper.paramValidationError({
+          internal_error_identifier: 'a_s_v_v_1',
+          api_error_identifier: 'invalid_api_params',
+          params_error_identifiers: ['invalid_per_reply_amount_in_wei'],
+          debug_options: { perReplyAmountInWei: oThis.perReplyAmountInWei }
+        })
+      );
+    }
   }
 }
 
