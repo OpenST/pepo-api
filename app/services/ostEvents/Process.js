2dd8c11a08209d248935dc710d14dbee0e332c1f
aman-barbaria <amanbarbaria@gmail.com>
2019-12-03 18:57:42 +0530

    send pagerduty on ost event failed status

--- app/services/ostEvents/Process.js
+++ app/services/ostEvents/Process.js
@@ -5,6 +5,8 @@ const rootPrefix = '../../..',
   OstEventProcessFactory = require(rootPrefix + '/app/services/ostEvents/Factory'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   logger = require(rootPrefix + '/lib/logger/customConsoleLogger'),
+  createErrorLogsEntry = require(rootPrefix + '/lib/errorLogs/createEntry'),
+  errorLogsConstants = require(rootPrefix + '/lib/globalConstant/errorLogs'),
   ostEventConstants = require(rootPrefix + '/lib/globalConstant/ostEvent');
 
 /**
@@ -146,7 +148,7 @@ class OstEventProcess extends ServiceBase {
     if (response.isSuccess()) {
       await oThis._updateOstEventStatus(ostEventConstants.doneStatus);
     } else {
-      //todo: create error log
+      await createErrorLogsEntry.perform(response, errorLogsConstants.mediumSeverity);
       await oThis._updateOstEventStatus(ostEventConstants.failedStatus);
     }
   }
