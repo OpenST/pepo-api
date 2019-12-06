c8b2366ac2d5dd5c368ab48f51e37c8c41f94a15
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2019-12-06 16:08:44 +0530

    Migration changes.

--- db/migration/1575617349748_addIsPopularLastReplyTimeInFeeds.js
+++ db/migration/1575617349748_addIsPopularLastReplyTimeInFeeds.js
@@ -8,10 +8,10 @@ const dbKind = DbKindConstant.sqlDbKind;
 
 const upQuery =
   'ALTER TABLE `feeds` \n\
-      ADD COLUMN `is_popular` tinyint(4) AFTER `actor`, \n\
+      ADD COLUMN `is_popular` tinyint(4) NOT NULL DEFAULT 0 AFTER `actor`, \n\
       ADD COLUMN `last_reply_timestamp` int(11) AFTER `is_popular`;';
 
-const downQuery = 'ALTER TABLE `feeds` DROP `is_popular`, DROP `last_reply_timestamp`;';
+const downQuery = 'ALTER TABLE `feeds` DROP `last_reply_timestamp`, DROP `is_popular`;';
 
 const addIsPopularLastReplyTimeInFeeds = {
   dbName: dbName,
