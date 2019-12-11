5f7354088b4fb794e95d2913edba0461d47ca997
alpeshvmodi <alpeshvmodi@gmail.com>
2019-12-11 13:31:16 +0530

    back populate pixels

---
+++ executables/oneTimers/backPopulateBecomeCreatorPixel.js
@@ -0,0 +1,129 @@
+const fs = require('fs');
+
+const rootPrefix = '../..',
+  UserModel = require(rootPrefix + '/app/models/mysql/User'),
+  ActivityLogModel = require(rootPrefix + '/app/models/mysql/AdminActivityLog'),
+  UserDeviceExtendedDetailModel = require(rootPrefix + '/app/models/mysql/UserDeviceExtendedDetail'),
+  adminActivityLogConstants = require(rootPrefix + '/lib/globalConstant/adminActivityLogs'),
+  pixelConstants = require(rootPrefix + '/lib/globalConstant/pixel'),
+  userConstants = require(rootPrefix + '/lib/globalConstant/user'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger');
+
+let userIdsBeforeThis = process.argv[2] || 0;
+const logPixelTemplate = `{{timestamp}} :: 114.143.238.58 :: GET /devp101_pixel.png?v=2&tid=back_populate&did={{device_id}}&sesid=back_populate&serid=1&dt=back_populate&ua=${
+  pixelConstants.pixelUserAgent
+}&ee=user&ea=creator_approved&pt=back_populate&uid={{current_admin_user_id}}&approved_user_id={{current_user_id}}&mobile_app_version={{mobile_app_version}} HTTP/1.1 :: - :: ${
+  pixelConstants.pixelUserAgent
+}\n`;
+
+let fileName = 'devp101_pixel_access.log-20191211.1575892201';
+if (process.env.PA_ENVIRONMENT == 'production') {
+  fileName = 'pp1001_pixel_access.log-20191211.1575892201';
+}
+const filePath = __dirname + '/../../' + fileName;
+
+class backPopulateBecomeCreatorPixel {
+  /**
+   * Perform.
+   *
+   * @returns {Promise<void>}
+   */
+  async perform() {
+    const oThis = this;
+
+    while (1) {
+      let userIds = [],
+        userToApproverAdminDetailsMap = {},
+        userToDeviceDataMap = {};
+
+      const userDbRows = await new UserModel()
+        .select('*')
+        .where(['id < ?', userIdsBeforeThis])
+        .limit(10)
+        .order_by('id Desc')
+        .fire();
+
+      if (userDbRows.length == 0) {
+        break;
+      }
+      for (let i = 0; i < userDbRows.length; i++) {
+        let user = userDbRows[i];
+        userIds.push(user.id);
+      }
+
+      const activityLogsResp = await new ActivityLogModel()
+        .select('*')
+        .where({ action_on: userIds })
+        .where(['action=?', adminActivityLogConstants.invertedActions[adminActivityLogConstants.approvedAsCreator]])
+        .fire();
+
+      for (let i = 0; i < activityLogsResp.length; i++) {
+        userToApproverAdminDetailsMap[activityLogsResp[i].action_on] = activityLogsResp[i];
+      }
+
+      const userDeviceExtResp = await new UserDeviceExtendedDetailModel()
+        .select('*')
+        .where({ user_id: userIds })
+        .order_by('id Desc')
+        .fire();
+
+      for (let i = 0; i < userDeviceExtResp.length; i++) {
+        userToDeviceDataMap[userDeviceExtResp[i].user_id] = userDeviceExtResp[i];
+      }
+
+      for (let i = 0; i < userDbRows.length; i++) {
+        let user = userDbRows[i],
+          timestamp = user.created_at,
+          current_user_id = user.id,
+          userApprovedBit = userConstants.invertedProperties[userConstants.isApprovedCreatorProperty];
+
+        if ((user.properties & userApprovedBit) != userApprovedBit) {
+          continue;
+        }
+
+        let mobile_app_version = userToDeviceDataMap[current_user_id]
+            ? userToDeviceDataMap[current_user_id].app_version
+            : '',
+          deviceId = userToDeviceDataMap[current_user_id] ? userToDeviceDataMap[current_user_id].device_id : '',
+          current_admin_user_id = userToApproverAdminDetailsMap[current_user_id]
+            ? userToApproverAdminDetailsMap[current_user_id].admin_id
+            : '';
+
+        let logline = logPixelTemplate
+          .replace('{{timestamp}}', timestamp)
+          .replace('{{current_user_id}}', current_user_id)
+          .replace('{{mobile_app_version}}', mobile_app_version)
+          .replace('{{current_admin_user_id}}', current_admin_user_id)
+          .replace('{{device_id}}', deviceId);
+
+        await oThis.appendLoglineToFile(logline, user.id);
+        userIdsBeforeThis = user.id;
+      }
+    }
+  }
+
+  async appendLoglineToFile(logline, userId) {
+    return new Promise(function(onResolve, onReject) {
+      console.log('filePath----------', filePath);
+      fs.appendFile(filePath, logline, function(err) {
+        if (err) {
+          logger.error(err);
+        } else {
+          logger.log('The log inserted for userId:' + userId);
+        }
+        onResolve();
+      });
+    });
+  }
+}
+
+new backPopulateBecomeCreatorPixel()
+  .perform()
+  .then(function() {
+    logger.win('All Devices recorded.');
+    process.exit(0);
+  })
+  .catch(function(err) {
+    logger.error('Devices recording failed. Error: ', err);
+    process.exit(1);
+  });
---
+++ executables/oneTimers/backPopulateRegisterUserPixel.js
@@ -0,0 +1,130 @@
+const fs = require('fs');
+
+const rootPrefix = '../..',
+  UserModel = require(rootPrefix + '/app/models/mysql/User'),
+  InviteCodeModel = require(rootPrefix + '/app/models/mysql/InviteCode'),
+  UserUtmDetailModel = require(rootPrefix + '/app/models/mysql/UserUtmDetail'),
+  pixelConstants = require(rootPrefix + '/lib/globalConstant/pixel'),
+  logger = require(rootPrefix + '/lib/logger/customConsoleLogger');
+
+let userIdsBeforeThis = process.argv[2] || 0;
+const logPixelTemplate = `{{timestamp}} :: 114.143.238.58 :: GET /devp101_pixel.png?v=2&tid=back_populate&serid=1&sesid=back_populate&dt=back_populate&ua=${
+  pixelConstants.pixelUserAgent
+}&ee=user&ea=registration&pt=back_populate&uid={{current_user_id}}&invite_code={{invite_code}}&registration_at={{registration_at}}&utm_source={{utm_source}}&utm_medium={{utm_medium}}&utm_campaign={{utm_campaign}} HTTP/1.1 :: - :: ${
+  pixelConstants.pixelUserAgent
+}\n`;
+
+let fileName = 'devp101_pixel_access.log-20191211.1575892201';
+if (process.env.PA_ENVIRONMENT == 'production') {
+  fileName = 'pp1001_pixel_access.log-20191211.1575892201';
+}
+const filePath = __dirname + '/../../' + fileName;
+
+class populateRegisterUserPixel {
+  /**
+   * Perform.
+   *
+   * @returns {Promise<void>}
+   */
+  async perform() {
+    const userUtmParams = {};
+
+    const utmDbRows = await new UserUtmDetailModel().select('*').fire();
+    for (let i = 0; i < utmDbRows.length; i++) {
+      let utmParams = utmDbRows[i];
+      userUtmParams[utmParams.user_id] = utmParams;
+    }
+
+    while (1) {
+      let userIds = [];
+
+      const userDbRows = await new UserModel()
+        .select('*')
+        .where(['id < ?', userIdsBeforeThis])
+        .limit(100)
+        .order_by('id Desc')
+        .fire();
+
+      if (userDbRows.length == 0) {
+        break;
+      }
+      for (let i = 0; i < userDbRows.length; i++) {
+        let user = userDbRows[i];
+        userIds.push(user.id);
+      }
+
+      const userToInviterUser = {},
+        inviterUserToCode = {},
+        inviterCodeIds = [];
+
+      const inviteCodeDbRows = await new InviteCodeModel()
+        .select('*')
+        .where({ user_id: userIds })
+        .fire();
+      for (let i = 0; i < inviteCodeDbRows.length; i++) {
+        let inviteCodeRow = inviteCodeDbRows[i];
+        userToInviterUser[inviteCodeRow.user_id] = inviteCodeRow.inviter_code_id;
+        inviterCodeIds.push(inviteCodeRow.inviter_code_id);
+      }
+      const inviterCodeDbRows = await new InviteCodeModel()
+        .select('*')
+        .where({ id: inviterCodeIds })
+        .fire();
+      for (let i = 0; i < inviterCodeDbRows.length; i++) {
+        let inviterCodeRow = inviterCodeDbRows[i];
+        inviterUserToCode[inviterCodeRow.id] = inviterCodeRow.code;
+      }
+
+      for (let i = 0; i < userDbRows.length; i++) {
+        let user = userDbRows[i],
+          timestamp = user.created_at,
+          current_user_id = user.id,
+          registration_at = timestamp;
+
+        let inviterCodeId = userToInviterUser[current_user_id];
+        let invite_code = inviterCodeId ? inviterUserToCode[inviterCodeId] : null;
+
+        let utmParams = userUtmParams[current_user_id],
+          utm_source = '',
+          utm_medium = '',
+          utm_campaign = '';
+
+        if (utmParams) {
+          utm_source = utmParams.utm_source;
+          utm_medium = utmParams.utm_medium;
+          utm_campaign = utmParams.utm_campaign;
+        }
+
+        let logline = logPixelTemplate
+          .replace('{{timestamp}}', timestamp)
+          .replace('{{current_user_id}}', current_user_id)
+          .replace('{{registration_at}}', registration_at)
+          .replace('{{invite_code}}', invite_code)
+          .replace('{{utm_source}}', utm_source)
+          .replace('{{utm_medium}}', utm_medium)
+          .replace('{{utm_campaign}}', utm_campaign);
+
+        console.log('filePath----------', filePath);
+        fs.appendFile(filePath, logline, function(err) {
+          if (err) {
+            logger.error(err);
+          } else {
+            userIdsBeforeThis = user.id;
+            logger.log('The log inserted for userId:' + userIdsBeforeThis);
+          }
+        });
+      }
+    }
+  }
+}
+
+new populateRegisterUserPixel()
+  .perform()
+  .then(function() {
+    logger.win('All Devices recorded.');
+    process.exit(0);
+  })
+  .catch(function(err) {
+    logger.error('Devices recording failed. Error: ', err);
+    process.exit(1);
+  });
