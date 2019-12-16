e657f087ccbda1c2b23a1f13beb1c480cbb5dd81
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2019-12-16 13:32:29 +0530

    Cache clear changes.

--- app/services/user/profile/update/FanVideo.js
+++ app/services/user/profile/update/FanVideo.js
@@ -70,6 +70,7 @@ class UpdateFanVideo extends UpdateProfileBase {
     oThis.flushUserCache = false;
     oThis.flushUserProfileElementsCache = false;
     oThis.mentionedUserIds = [];
+    oThis.feedId = null;
 
     oThis.paginationTimestamp = Math.round(new Date() / 1000);
   }
@@ -286,7 +287,7 @@ class UpdateFanVideo extends UpdateProfileBase {
   async _addFeed() {
     const oThis = this;
 
-    return new FeedModel()
+    const feedInsertResp = await new FeedModel()
       .insert({
         primary_external_entity_id: oThis.videoId,
         kind: feedsConstants.invertedKinds[feedsConstants.fanUpdateKind],
@@ -294,7 +295,8 @@ class UpdateFanVideo extends UpdateProfileBase {
         pagination_identifier: oThis.paginationTimestamp
       })
       .fire();
-    //todo feed: flush cache on id
+
+    oThis.feedId = feedInsertResp.id;
   }
 
   /**
@@ -309,7 +311,9 @@ class UpdateFanVideo extends UpdateProfileBase {
     const promisesArray = [];
 
     promisesArray.push(super._flushCaches());
-    promisesArray.push(FeedModel.flushCache({ paginationTimestamp: oThis.paginationTimestamp }));
+    if (oThis.feedId) {
+      promisesArray.push(FeedModel.flushCache({ id: oThis.feedId }));
+    }
     promisesArray.push(VideoDetailsModel.flushCache({ userId: oThis.profileUserId }));
 
     await Promise.all(promisesArray);
