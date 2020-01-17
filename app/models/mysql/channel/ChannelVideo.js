d14176073a6b0b20c0daf33ce16ac869ca86bca9
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-01-17 13:42:17 +0530

    Added channel video model and constants.

---
+++ app/models/mysql/channel/ChannelVideo.js
@@ -0,0 +1,68 @@
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database'),
+  channelVideosConstants = require(rootPrefix + '/lib/globalConstant/channel/channelVideos');
+
+// Declare variables names.
+const dbName = databaseConstants.channelDbName;
+
+/**
+ * Class for channel video model.
+ *
+ * @class ChannelVideoModel
+ */
+class ChannelVideoModel extends ModelBase {
+  /**
+   * Constructor for channels model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'channel_videos';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.channel_id
+   * @param {number} dbRow.video_id
+   * @param {number} dbRow.status
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @returns {{}}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      channelId: dbRow.channel_id,
+      videoId: dbRow.video_id,
+      status: channelVideosConstants.invertedStatuses[dbRow.status],
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Flush cache.
+   *
+   * @returns {Promise<*>}
+   */
+  static async flushCache() {
+    // Do nothing.
+  }
+}
+
+module.exports = ChannelVideoModel;
