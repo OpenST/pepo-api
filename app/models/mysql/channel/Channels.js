a513a10e494d588f45e7ef5592249061c9011201
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-01-17 13:29:55 +0530

    Channels table model and constants.

---
+++ app/models/mysql/channel/Channels.js
@@ -0,0 +1,70 @@
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database'),
+  channelConstants = require(rootPrefix + '/lib/globalConstant/channel/channels');
+
+// Declare variables names.
+const dbName = databaseConstants.channelDbName;
+
+/**
+ * Class for channels model.
+ *
+ * @class ChannelsModel
+ */
+class ChannelsModel extends ModelBase {
+  /**
+   * Constructor for channels model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'channels';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.name
+   * @param {number} dbRow.status
+   * @param {number} dbRow.description_id
+   * @param {number} dbRow.image_id
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @returns {{}}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      name: dbRow.name,
+      status: channelConstants.invertedStatuses[dbRow.status],
+      descriptionId: dbRow.description_id,
+      imageId: dbRow.image_id,
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Flush cache.
+   *
+   * @returns {Promise<*>}
+   */
+  static async flushCache() {
+    // Do nothing.
+  }
+}
+
+module.exports = ChannelsModel;
