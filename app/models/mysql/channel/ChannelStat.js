13846b2ce4cdbb2386be7bbe700483644f7db559
shlokgilda <shlok@ost.com>
2020-01-17 13:10:44 +0530

    Updated channel stats model.

---
+++ app/models/mysql/channel/ChannelStat.js
@@ -0,0 +1,83 @@
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+
+// Declare variables names.
+const dbName = databaseConstants.channelDbName;
+
+/**
+ * Class for channel stats model.
+ *
+ * @class ChannelStatModel
+ */
+class ChannelStatModel extends ModelBase {
+  /**
+   * Constructor for channel stats model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'channel_stats';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.channel_id
+   * @param {number} dbRow.total_videos
+   * @param {number} dbRow.total_users
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @returns {{}}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      channelId: dbRow.channel_id,
+      totalVideos: dbRow.total_videos,
+      totalUsers: dbRow.total_users,
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Get channel stats by channel id.
+   *
+   * @param {number} channelId
+   *
+   * @returns {Promise<any>}
+   */
+  async getByChannelId(channelId) {
+    const oThis = this;
+
+    return oThis
+      .select('*')
+      .where({ channel_id: channelId })
+      .fire();
+  }
+
+  /**
+   * Flush cache.
+   *
+   * @returns {Promise<*>}
+   */
+  static async flushCache() {
+    // Do nothing.
+  }
+}
+
+module.exports = ChannelStatModel;
