e0522fdf58e0f7808414cd0e4a550708ff515e82
shlokgilda <shlok@ost.com>
2020-01-17 13:20:51 +0530

    Added channel tag model.

---
+++ app/models/mysql/channel/ChannelTag.js
@@ -0,0 +1,68 @@
+const rootPrefix = '../../../..',
+  ModelBase = require(rootPrefix + '/app/models/mysql/Base'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database'),
+  channelTagsConstants = require(rootPrefix + '/lib/globalConstant/channel/channelTags');
+
+// Declare variables names.
+const dbName = databaseConstants.channelDbName;
+
+/**
+ * Class for channel tags model.
+ *
+ * @class ChannelTagModel
+ */
+class ChannelTagModel extends ModelBase {
+  /**
+   * Constructor for channel tags model.
+   *
+   * @augments ModelBase
+   *
+   * @constructor
+   */
+  constructor() {
+    super({ dbName: dbName });
+
+    const oThis = this;
+
+    oThis.tableName = 'channel_stats';
+  }
+
+  /**
+   * Format Db data.
+   *
+   * @param {object} dbRow
+   * @param {number} dbRow.id
+   * @param {number} dbRow.channel_id
+   * @param {number} dbRow.tag_id
+   * @param {number} dbRow.status
+   * @param {number} dbRow.created_at
+   * @param {number} dbRow.updated_at
+   *
+   * @returns {{}}
+   */
+  formatDbData(dbRow) {
+    const oThis = this;
+
+    const formattedData = {
+      id: dbRow.id,
+      channelId: dbRow.channel_id,
+      tagId: dbRow.tag_id,
+      status: channelTagsConstants.statuses[dbRow.status],
+      createdAt: dbRow.created_at,
+      updatedAt: dbRow.updated_at
+    };
+
+    return oThis.sanitizeFormattedData(formattedData);
+  }
+
+  /**
+   * Flush cache.
+   *
+   * @returns {Promise<*>}
+   */
+  static async flushCache() {
+    // Do nothing.
+  }
+}
+
+module.exports = ChannelTagModel;
