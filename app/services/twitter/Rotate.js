8fc63256dfe84d5e937795e07f92989221688ed9
Dhananjay8 <dhananjay@ost.com>
2020-01-20 15:37:36 +0530

    rotate api changes

--- app/services/twitter/Rotate.js
+++ app/services/twitter/Rotate.js
@@ -3,6 +3,7 @@ const rootPrefix = '../../..',
   UserModel = require(rootPrefix + '/app/models/mysql/User'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   TwitterUserModel = require(rootPrefix + '/app/models/mysql/TwitterUser'),
+  UserUniqueIdentifierModel = require(rootPrefix + '/app/models/mysql/UserIdentifier'),
   TwitterUserExtendedModel = require(rootPrefix + '/app/models/mysql/TwitterUserExtended'),
   UserIdByUserNamesCache = require(rootPrefix + '/lib/cacheManagement/multi/UserIdByUserNames'),
   TwitterUserByIdsCache = require(rootPrefix + '/lib/cacheManagement/multi/TwitterUserByIds'),
@@ -58,6 +59,8 @@ class RotateTwitterAccount extends ServiceBase {
     await oThis._deleteTwitterUserExtended();
 
     await oThis._markUserEmailAsNull();
+
+    await oThis._deleteUniqueUserIdentifier();
 
     return responseHelper.successWithData({});
   }
@@ -226,6 +229,21 @@ class RotateTwitterAccount extends ServiceBase {
       .fire();
 
     await UserModel.flushCache({ id: oThis.userId });
+  }
+
+  /**
+   * Delte user identifier.
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _deleteUniqueUserIdentifier() {
+    const oThis = this;
+
+    await new UserUniqueIdentifierModel()
+      .delete()
+      .where({ user_id: oThis.userId })
+      .fire();
   }
 }
 
