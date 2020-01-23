30739ef11bfe2d47814e3246f662e34684f935e8
AnaghaMurtarkar <murtarkaranagha@gmail.com>
2020-01-23 19:26:46 +0530

    Minor changes.

--- db/migration/1579787451115_addIndexOnGoogleUsers.js
+++ db/migration/1579787451115_addIndexOnGoogleUsers.js
@@ -11,11 +11,11 @@ const upQuery2 = 'ALTER TABLE `google_users` ADD UNIQUE INDEX `uidx_2` (`user_id
 const downQuery1 = 'ALTER TABLE `google_users` DROP INDEX `uidx_1`;';
 const downQuery2 = 'ALTER TABLE `google_users` DROP INDEX `uidx_2`;';
 
-const addUniqIndexInUserIdentifiers = {
+const addIndexOnGoogleUsers = {
   dbName: dbName,
   up: [upQuery1, upQuery2],
   down: [downQuery2, downQuery1],
   dbKind: dbKind
 };
 
-module.exports = addUniqIndexInUserIdentifiers;
+module.exports = addIndexOnGoogleUsers;
