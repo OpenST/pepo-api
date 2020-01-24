546ff96cdc50681ff6b3f66dc562dcf7aafb45a2
shlokgilda <shlok@ost.com>
2020-01-24 19:33:55 +0530

    Updated channel get service.

--- app/services/channel/Get.js
+++ app/services/channel/Get.js
@@ -3,8 +3,11 @@ const rootPrefix = '../../..',
   CommonValidators = require(rootPrefix + '/lib/validators/Common'),
   ChannelByIdsCache = require(rootPrefix + '/lib/cacheManagement/multi/channel/ChannelByIds'),
   ChannelStatByChannelIdsCache = require(rootPrefix + '/lib/cacheManagement/multi/channel/ChannelStatByChannelIds'),
+  ChannelUserByUserIdAndChannelIdsCache = require(rootPrefix +
+    '/lib/cacheManagement/multi/channel/ChannelUserByUserIdAndChannelIds'),
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
-  channelConstants = require(rootPrefix + '/lib/globalConstant/channel/channels');
+  channelConstants = require(rootPrefix + '/lib/globalConstant/channel/channels'),
+  channelUsersConstants = require(rootPrefix + '/lib/globalConstant/channel/channelUsers');
 
 /**
  * Class to get channel details.
@@ -29,10 +32,11 @@ class GetChannel extends ServiceBase {
     const oThis = this;
 
     oThis.channelId = params.channel_id;
-    oThis.currentUser = params.current_user;
+    oThis.currentUser = params.current_user || {};
 
     oThis.channelDetails = {};
     oThis.channelStats = {};
+    oThis.currentUserChannelRelations = {};
   }
 
   async _asyncPerform() {
@@ -61,11 +65,11 @@ class GetChannel extends ServiceBase {
       return Promise.reject(cacheResponse);
     }
 
-    oThis.channelDetails = cacheResponse.data[oThis.channelId];
+    const channelDetails = cacheResponse.data[oThis.channelId];
 
     if (
-      !CommonValidators.validateNonEmptyObject(oThis.channelDetails) ||
-      oThis.channelDetails.status !== channelConstants.activeStatus
+      !CommonValidators.validateNonEmptyObject(channelDetails) ||
+      channelDetails.status !== channelConstants.activeStatus
     ) {
       return Promise.reject(
         responseHelper.error({
@@ -73,11 +77,13 @@ class GetChannel extends ServiceBase {
           api_error_identifier: 'entity_not_found',
           debug_options: {
             channelId: oThis.channelId,
-            channelDetails: oThis.channelDetails
+            channelDetails: channelDetails
           }
         })
       );
     }
+
+    oThis.channelDetails = channelDetails;
   }
 
   /**
@@ -96,7 +102,57 @@ class GetChannel extends ServiceBase {
       return Promise.reject(cacheResponse);
     }
 
-    oThis.channelStats = cacheResponse.data[oThis.channelId];
+    const channelStats = cacheResponse.data[oThis.channelId];
+
+    if (!CommonValidators.validateNonEmptyObject(channelStats)) {
+      return Promise.reject(
+        responseHelper.error({
+          internal_error_identifier: 'a_s_c_g_2',
+          api_error_identifier: 'entity_not_found',
+          debug_options: {
+            channelId: oThis.channelId,
+            channelStats: channelStats
+          }
+        })
+      );
+    }
+
+    oThis.channelStats = channelStats;
+  }
+
+  /**
+   * Fetch current user channel relations.
+   *
+   * @sets oThis.currentUserChannelRelations
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _fetchUserChannelRelations() {
+    const oThis = this;
+
+    if (!oThis.currentUser.id) {
+      return;
+    }
+
+    oThis.currentUserChannelRelations = { [oThis.channelId]: { is_member: 0, has_muted: 0, has_blocked: 0 } };
+
+    const userId = oThis.currentUser.id;
+    const cacheResponse = await new ChannelUserByUserIdAndChannelIdsCache({
+      userId: userId,
+      channelIds: [oThis.channelId]
+    }).fetch();
+    if (cacheResponse.isFailure()) {
+      return Promise.reject(cacheResponse);
+    }
+
+    const channelUserRelation = cacheResponse.data[oThis.channelId];
+    if (
+      CommonValidators.validateNonEmptyObject(channelUserRelation) &&
+      channelUserRelation.status === channelUsersConstants.activeStatus
+    ) {
+      oThis.currentUserChannelRelations[oThis.channelId].is_member = 1;
+    }
   }
 }
 
