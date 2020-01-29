ca79723be89451b98b09cc4929034a256776c352
Tejas-Sangani <tejas@ost.com>
2020-01-29 19:55:11 +0530

    migration to add permalink in channel table and unique index on permalink

--- db/migration/1579245407038_createChannels.js
+++ db/migration/1579245407038_createChannels.js
@@ -6,17 +6,19 @@ const dbName = database.channelDbName;
 const dbKind = DbKindConstant.sqlDbKind;
 
 const upQuery =
-  'CREATE TABLE `channels` (\
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,\
+  'CREATE TABLE `channels` (\n\
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,\n\
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL, \n\
-  `status` tinyint(4) NOT NULL,\
+  `status` tinyint(4) NOT NULL,\n\
   `tagline_id` bigint(20) DEFAULT NULL,\n\
   `description_id` bigint(20) DEFAULT NULL,\n\
   `cover_image_id` bigint(20) DEFAULT NULL,\n\
-  `created_at` int(11) NOT NULL,\
-  `updated_at` int(11) NOT NULL,\
-  PRIMARY KEY (`id`),\
-  UNIQUE uidx_1 (`name`)\
+  `permalink` varchar(50) NOT NULL, \n\
+  `created_at` int(11) NOT NULL,\n\
+  `updated_at` int(11) NOT NULL,\n\
+  PRIMARY KEY (`id`),\n\
+  UNIQUE uidx_1 (`name`),\n\
+  UNIQUE uidx_2 (`permalink`) \n\
   ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
 
 const downQuery = 'drop table if exists `channels`;';
