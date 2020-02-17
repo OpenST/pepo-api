4f2a0571eb3fd7b478daffc43939cd47de935c9e
Dhananjay8 <dhananjay@ost.com>
2020-02-17 20:15:20 +0530

    store related changes

--- app/services/redemption/InitiateRequest.js
+++ app/services/redemption/InitiateRequest.js
@@ -218,8 +218,8 @@ class InitiateRequestRedemption extends ServiceBase {
       let apiErrorIdentifier = null;
 
       switch (Number(oThis.productMinDollarValue)) {
-        case 10:
-          apiErrorIdentifier = 'min_redemption_amount_10';
+        case 15:
+          apiErrorIdentifier = 'min_redemption_amount_15';
           break;
         case 25:
           apiErrorIdentifier = 'min_redemption_amount_25';
