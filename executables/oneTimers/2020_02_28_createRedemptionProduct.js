6790f9dd4430ff72d06640f2e706aeb2da4b69fb
sunil <sunil@hem.com>
2020-03-04 01:05:38 +0530

    Added max dollar value and display order for redemption products

--- executables/oneTimers/2020_02_28_createRedemptionProduct.js
+++ executables/oneTimers/2020_02_28_createRedemptionProduct.js
@@ -13,7 +13,9 @@ program
   .option('--prodDescription <prodDescription>', 'Product description')
   .option('--dollarValue <dollarValue>', 'Product dollar value')
   .option('--minDollarValue <minDollarValue>', 'Product min dollar value')
+  .option('--maxDollarValue <maxDollarValue>', 'Product max dollar value. Pass 0, if no max limit.')
   .option('--dollarStep <dollarStep>', 'Product dollar step')
+  .option('--displayOrder <displayOrder>', 'Product display order. Default is 1')
   .parse(process.argv);
 
 program.on('--help', function() {
@@ -21,7 +23,7 @@ program.on('--help', function() {
   logger.log('  Example:');
   logger.log('');
   logger.log(
-    '    node executables/oneTimers/2020_02_28_createRedemptionProduct --productKind "UNSTOPPABLE DOMAINS" --images \'{"square":"https://d3attjoi5jlede.cloudfront.net/images/web/redemption/redemption-uber-1x1.png","landscape":"https://d3attjoi5jlede.cloudfront.net/images/web/redemption/redemption-uber-16x9.png"}\' --prodDescription \'Blockchain domains replace crypto addresses with human readable names and can be used to build censorship resistant websites. Get your code to purchase <a href="https://unstoppabledomains.com/pepo-preview/" rel="noopener noreferrer nofollow" title="Unstoppable domains"> Unstoppable domains </a>\' --dollarValue 10.00 --minDollarValue 10.00 --dollarStep 1.00'
+  '    node executables/oneTimers/2020_02_28_createRedemptionProduct --productKind "UNSTOPPABLE DOMAINS" --images \'{"square":"https://d3attjoi5jlede.cloudfront.net/images/web/redemption/redemption-uber-1x1.png","landscape":"https://d3attjoi5jlede.cloudfront.net/images/web/redemption/redemption-uber-16x9.png"}\' --prodDescription \'Blockchain domains replace crypto addresses with human readable names and can be used to build censorship resistant websites. Get your code to purchase <a href="https://unstoppabledomains.com/pepo-preview/" rel="noopener noreferrer nofollow" title="Unstoppable domains"> Unstoppable domains </a>\' --dollarValue 5.00 --minDollarValue 5.00 --maxDollarValue 0 --dollarStep 1.00 --displayOrder 1.12'
   );
   logger.log('');
   logger.log('');
@@ -33,7 +35,9 @@ if (!program.productKind) {
     !program.images &&
     !program.dollarValue &&
     !program.minDollarValue &&
-    !program.dollarStep
+    !program.maxDollarValue &&
+    !program.dollarStep &&
+    !program.displayOrder
   ) {
     program.help();
     process.exit(1);
@@ -48,7 +52,9 @@ class CreateProducts {
     oThis.images = params.images;
     oThis.dollarValue = params.dollarValue;
     oThis.minDollarValue = params.minDollarValue;
+    oThis.maxDollarValue = params.maxDollarValue;
     oThis.dollarStep = params.dollarStep;
+    oThis.displayOrder = params.displayOrder;
   }
 
   async perform() {
@@ -78,8 +84,14 @@ class CreateProducts {
       if (oThis.minDollarValue) {
         params['min_dollar_value'] = oThis.minDollarValue;
       }
+      if (oThis.maxDollarValue) {
+        params['max_dollar_value'] = oThis.maxDollarValue > 0 ? oThis.maxDollarValue : null;
+      }
       if (oThis.dollarStep) {
         params['dollar_step'] = oThis.dollarStep;
+      }
+      if (oThis.displayOrder) {
+        params['display_order'] = oThis.displayOrder;
       }
 
       if (CommonValidators.validateNonEmptyObject(params)) {
@@ -96,7 +108,9 @@ class CreateProducts {
           description: oThis.description,
           dollar_value: oThis.dollarValue,
           min_dollar_value: oThis.minDollarValue,
+          max_dollar_value: oThis.maxDollarValue > 0 ? oThis.maxDollarValue : null,
           dollar_step: oThis.dollarStep,
+          display_order: oThis.displayOrder || 1,
           status: redemptionConstants.invertedStatuses[redemptionConstants.activeStatus]
         })
         .fire();
@@ -133,6 +147,7 @@ class CreateProducts {
     if (
       !CommonValidators.validateNumber(oThis.dollarValue) ||
       !CommonValidators.validateNumber(oThis.minDollarValue) ||
+      !CommonValidators.validateNumber(oThis.maxDollarValue) ||
       !CommonValidators.validateNumber(oThis.dollarStep)
     ) {
       return Promise.reject(
@@ -142,7 +157,21 @@ class CreateProducts {
           debug_options: {
             dollarValue: oThis.dollarValue,
             minDollarValue: oThis.minDollarValue,
+            maxDollarValue: oThis.maxDollarValue,
             dollarStep: oThis.dollarStep
+          }
+        })
+      );
+    }
+
+    if (oThis.maxDollarValue > 0 && oThis.minDollarValue > oThis.maxDollarValue) {
+      return Promise.reject(
+        responseHelper.error({
+          internal_error_identifier: 'e_ot_crp_3',
+          api_error_identifier: 'something_went_wrong',
+          debug_options: {
+            minDollarValue: oThis.minDollarValue,
+            maxDollarValue: oThis.maxDollarValue
           }
         })
       );
@@ -152,7 +181,7 @@ class CreateProducts {
       if (!CommonValidators.isExternalLinkSafe(oThis.description)) {
         return Promise.reject(
           responseHelper.error({
-            internal_error_identifier: 'e_ot_crp_3',
+            internal_error_identifier: 'e_ot_crp_4',
             api_error_identifier: 'something_went_wrong',
             debug_options: { description: oThis.description }
           })
@@ -168,7 +197,9 @@ new CreateProducts({
   description: program.prodDescription,
   dollarValue: program.dollarValue,
   minDollarValue: program.minDollarValue,
-  dollarStep: program.dollarStep
+  maxDollarValue: program.maxDollarValue,
+  dollarStep: program.dollarStep,
+  displayOrder: program.displayOrder
 })
   .perform()
   .then(function() {
