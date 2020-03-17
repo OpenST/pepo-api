ee21a249a237104b43ab0bbbc0307cb916fb9f8b
aman-barbaria <amanbarbaria@gmail.com>
2020-03-17 14:17:28 +0530

    script changes for marking users as unapproved

--- executables/oneTimers/dataMigrations/markAsUserUnapproved.js
+++ executables/oneTimers/dataMigrations/markAsUserUnapproved.js
@@ -8,9 +8,15 @@
 const command = require('commander');
 
 const rootPrefix = '../../..',
+  DeleteReplyVideoLib = require(rootPrefix + '/lib/video/delete/ReplyVideos'),
+  DeleteUserVideosLib = require(rootPrefix + '/lib/video/delete/UserVideos'),
   ReplyDetailModel = require(rootPrefix + '/app/models/mysql/ReplyDetail'),
   UserMuteModel = require(rootPrefix + '/app/models/mysql/UserMute'),
   VideoDetailModel = require(rootPrefix + '/app/models/mysql/VideoDetail'),
+  InviteCodeModel = require(rootPrefix + '/app/models/mysql/InviteCode'),
+  AdminActivityLogModel = require(rootPrefix + '/app/models/mysql/admin/AdminActivityLog'),
+  inviteCodeConstants = require(rootPrefix + '/lib/globalConstant/inviteCode'),
+  adminActivityLogConstants = require(rootPrefix + '/lib/globalConstant/admin/adminActivityLogs'),
   userConstants = require(rootPrefix + '/lib/globalConstant/user'),
   replyDetailConstants = require(rootPrefix + '/lib/globalConstant/replyDetail'),
   videoDetailsConstants = require(rootPrefix + '/lib/globalConstant/videoDetail'),
@@ -27,6 +33,8 @@ class MarkAsUserUnapproved {
     const oThis = this;
     oThis.userWithContent = {};
     oThis.userIdsToMarkAsUnapproved = [];
+    oThis.replyDetailIds = [];
+    oThis.videoIds = [];
   }
 
   async perform() {
@@ -34,16 +42,164 @@ class MarkAsUserUnapproved {
 
     await oThis._fetchUsers();
 
-    logger.log('MutedUsersWithContent=============', oThis.userWithContent);
-    logger.log('userIdsToMarkAsUnapproved=============', oThis.userIdsToMarkAsUnapproved);
-    //  todo: mark userIdsToMarkAsUnapproved as unmuted and unapproved.
+    await oThis._unapproveUsers();
+
+    // await oThis._deleteContentForApprovedUsers();
+
+    // await oThis._deleteReplyForUnapprovedUsers();
+  }
+
+  /**
+   * Delete content from users
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _deleteReplyForUnapprovedUsers() {
+    const oThis = this;
+
+    logger.log('Fetching unapproved User');
+
+    let unapprovedUserIds = [];
+
+    let usersData = await new UserModel()
+      .select('id')
+      .where({ status: userConstants.invertedStatuses[userConstants.activeStatus] })
+      .where('properties & 4 !=4')
+      .fire();
+
+    for (let index = 0; index < usersData.length; index++) {
+      const dbRow = usersData[index];
+      unapprovedUserIds.push(dbRow.id);
+    }
+
+    logger.log('unapprovedUserIds count:', unapprovedUserIds.length);
+
+    if (unapprovedUserIds.length == 0) {
+      return;
+    }
+
+    const replyDetailMapByUserId = {};
+
+    let queryObject1 = await new ReplyDetailModel()
+      .select(['id', 'creator_user_id'])
+      .where({
+        creator_user_id: unapprovedUserIds,
+        status: replyDetailConstants.invertedStatuses[replyDetailConstants.activeStatus]
+      })
+      .fire();
+
+    for (let index = 0; index < queryObject1.length; index++) {
+      const dbRow = queryObject1[index];
+      replyDetailMapByUserId[dbRow.creator_user_id] = replyDetailMapByUserId[dbRow.creator_user_id] || [];
+      replyDetailMapByUserId[dbRow.creator_user_id].push(dbRow.id);
+    }
+
+    logger.log('replyDetailMapByUserId:', replyDetailMapByUserId);
+
+    for (let userId in replyDetailMapByUserId) {
+      const replyDetailIds = replyDetailMapByUserId[userId];
+      logger.log(`\n\n replyDetailIds BATCH to DELETE For Unapproved Users=${userId}============`, replyDetailIds);
+
+      await new DeleteReplyVideoLib({
+        replyDetailsIds: replyDetailIds,
+        isUserAction: false,
+        currentAdminId: 0,
+        userId: userId
+      }).perform();
+    }
+  }
+
+  /**
+   * Delete content from users
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _deleteContentForApprovedUsers() {
+    const oThis = this;
+
+    logger.log('\n\nMutedUsersWithContent=============', oThis.userWithContent);
+
+    for (let userId in oThis.userWithContent) {
+      const replyDetailIds = oThis.userWithContent[userId].replyDetailIds;
+      const videoIds = oThis.userWithContent[userId].videoIds;
+
+      if (replyDetailIds.length > 0) {
+        logger.log('\n\n replyDetailIds BATCH to DELETE=============', replyDetailIds);
+        await new DeleteReplyVideoLib({
+          replyDetailsIds: replyDetailIds,
+          isUserAction: false,
+          currentAdminId: 0,
+          userId: userId
+        }).perform();
+      }
+
+      if (videoIds.length > 0) {
+        logger.log('\n\n videoIds BATCH to DELETE=============', videoIds);
+        await new DeleteUserVideosLib({
+          userId: userId,
+          currentAdminId: 0,
+          videoIds: videoIds,
+          isUserCreator: true
+        }).perform();
+      }
+    }
+  }
+
+  /**
+   * Unapprove users
+   *
+   * @returns {Promise<void>}
+   * @private
+   */
+  async _unapproveUsers() {
+    const oThis = this;
+
+    logger.log('\n\nuserIdsToMarkAsUnapproved=============', oThis.userIdsToMarkAsUnapproved);
+
+    const batchSize = 25;
+
+    for (let i = 0; i < oThis.userIdsToMarkAsUnapproved.length; i = i + batchSize) {
+      const batchUserIds = oThis.userIdsToMarkAsUnapproved.slice(i, i + batchSize);
+
+      logger.log('BATCH USER IDS TO UNAPPROVE=============', batchUserIds);
+
+      await new UserModel()
+        .update('properties = properties ^ 4')
+        .where({
+          id: batchUserIds,
+          status: userConstants.invertedStatuses[userConstants.activeStatus]
+        })
+        .where('properties & 4 =4')
+        .fire();
+
+      await new InviteCodeModel()
+        .update({
+          invite_limit: inviteCodeConstants.inviteMaxLimit
+        })
+        .where({ user_id: batchUserIds })
+        .fire();
+
+      const promises = [];
+
+      for (let index = 0; index < batchUserIds.length; index++) {
+        const userId = batchUserIds[index];
+        const promise = new AdminActivityLogModel().insertAction({
+          adminId: 0,
+          actionOn: userId,
+          action: adminActivityLogConstants.unapprovedAsCreator
+        });
+        promises.push(promise);
+      }
+
+      await Promise.all(promises);
+    }
   }
 
   /**
    * Fetch users
    *
-   * @param limit
-   * @param offset
    * @returns {Promise<void>}
    * @private
    */
@@ -114,11 +270,16 @@ class MarkAsUserUnapproved {
 
     for (let index = 0; index < queryObject1.length; index++) {
       const dbRow = queryObject1[index];
-      oThis.userWithContent[dbRow.creator_user_id] = 1;
+      oThis.userWithContent[dbRow.creator_user_id] = oThis.userWithContent[dbRow.creator_user_id] || {
+        replyDetailIds: [],
+        videoIds: []
+      };
+      oThis.userWithContent[dbRow.creator_user_id]['replyDetailIds'].push(dbRow.id);
+      oThis.replyDetailIds.push(dbRow.id);
     }
 
     let queryObject2 = await new VideoDetailModel()
-      .select('id, creator_user_id')
+      .select('id, creator_user_id, video_id')
       .where({
         creator_user_id: userIds,
         status: videoDetailsConstants.invertedStatuses[videoDetailsConstants.activeStatus]
@@ -127,7 +288,12 @@ class MarkAsUserUnapproved {
 
     for (let index = 0; index < queryObject2.length; index++) {
       const dbRow = queryObject2[index];
-      oThis.userWithContent[dbRow.creator_user_id] = 1;
+      oThis.userWithContent[dbRow.creator_user_id] = oThis.userWithContent[dbRow.creator_user_id] || {
+        replyDetailIds: [],
+        videoIds: []
+      };
+      oThis.userWithContent[dbRow.creator_user_id]['videoIds'].push(dbRow.video_id);
+      oThis.videoIds.push(dbRow.video_id);
     }
 
     for (let index = 0; index < userIds.length; index++) {
