3e74c09e1efc7f8579d66740c39505103cace504
abhayks1 <abhay@ost.com>
2020-03-24 19:15:06 +0530

    set meetings table auto increment to be 10000

---
+++ db/migration/1585056615054_updateMeetingAutoIncrement.js
@@ -0,0 +1,19 @@
+const rootPrefix = '../..',
+  dbKindConstants = require(rootPrefix + '/lib/globalConstant/dbKind'),
+  databaseConstants = require(rootPrefix + '/lib/globalConstant/database');
+
+const dbName = databaseConstants.meetingDbName;
+const dbKind = dbKindConstants.sqlDbKind;
+
+const upQuery = 'ALTER TABLE `meetings` AUTO_INCREMENT = 10000';
+
+const downQuery = 'ALTER TABLE `meetings` AUTO_INCREMENT = 1;';
+
+const migrationName = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery],
+  dbKind: dbKind
+};
+
+module.exports = migrationName;
