e63bffeaf9a8284bce1c3ea4a49073b7d35f5717
Shlok <gildashlok@hotmail.com>
2020-03-27 16:07:26 +0530

    Minor changes.

--- app/services/admin/channel/Edit.js
+++ app/services/admin/channel/Edit.js
@@ -1,15 +1,32 @@
 const rootPrefix = '../../../..',
   ServiceBase = require(rootPrefix + '/app/services/Base');
 
+// Declare constants.
 const ORIGIN_IMAGE_WIDTH = 1500;
 const ORIGIN_IMAGE_HEIGHT = 642;
 const SHARE_IMAGE_WIDTH = 1500;
 const SHARE_IMAGE_HEIGHT = 750;
+
+/**
+ * Class to edit channel.
+ *
+ * @class EditChannel
+ */
 class EditChannel extends ServiceBase {
+  /**
+   * Constructor to edit channel.
+   *
+   * @param {object} params
+   *
+   * @augments ServiceBase
+   *
+   * @constructor
+   */
   constructor(params) {
     super(params);
 
     const oThis = this;
+
     oThis.isEdit = params.isEdit;
     oThis.name = params.name;
     oThis.description = params.description;
@@ -18,6 +35,10 @@ class EditChannel extends ServiceBase {
     oThis.admins = params.admins;
     oThis.originalImage = params.originalImage;
     oThis.shareImage = params.shareImage;
+  }
+
+  async _asyncPerform() {
+    const oThis = this;
   }
 }
 
