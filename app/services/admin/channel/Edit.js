908dc264ce6ad6463b72262b2e12e9988e6ef0d7
kreloaded <kiran@ost.com>
2020-03-27 16:05:57 +0530

    Added params in constructor and constants

--- app/services/admin/channel/Edit.js
+++ app/services/admin/channel/Edit.js
@@ -1,5 +1,24 @@
-const rootPrefix = '../../../..';
+const rootPrefix = '../../../..',
+  ServiceBase = require(rootPrefix + '/app/services/Base');
 
-class EditChannel {}
+const ORIGIN_IMAGE_WIDTH = 1500;
+const ORIGIN_IMAGE_HEIGHT = 642;
+const SHARE_IMAGE_WIDTH = 1500;
+const SHARE_IMAGE_HEIGHT = 750;
+class EditChannel extends ServiceBase {
+  constructor(params) {
+    super(params);
+
+    const oThis = this;
+    oThis.isEdit = params.isEdit;
+    oThis.name = params.name;
+    oThis.description = params.description;
+    oThis.tagLine = params.tagLine;
+    oThis.permalink = params.permalink;
+    oThis.admins = params.admins;
+    oThis.originalImage = params.originalImage;
+    oThis.shareImage = params.shareImage;
+  }
+}
 
 module.exports = EditChannel;
