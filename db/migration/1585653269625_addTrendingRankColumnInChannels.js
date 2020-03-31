383ab2bcaee020e9734bead94f0a8f465f6894b6
sarvesh <sarvesh@ost.com>
2020-03-31 17:29:57 +0530

    Added migrations for column & index on trending_rank in channels, added index for name in channels and added index for is_live in meetings

---
+++ db/migration/1585653269625_addTrendingRankColumnInChannels.js
@@ -0,0 +1,24 @@
+const rootPrefix = '../..',
+  database = require(rootPrefix + '/lib/globalConstant/database'),
+  DbKindConstant = require(rootPrefix + '/lib/globalConstant/dbKind');
+
+const dbName = database.channelDbName;
+const dbKind = DbKindConstant.sqlDbKind;
+
+const upQuery =
+  'ALTER TABLE `channels` \n\
+      ADD COLUMN `trending_rank` bigint(20) NULL AFTER `permalink`, \n\
+      ADD INDEX idx_trending_rank (`trending_rank`) \n\
+      ';
+
+const downQuery = 'ALTER TABLE `channels` \n\
+  DROP COLUMN `trending_rank` , \n\
+  DROP INDEX `idx_trending_rank`';
+
+const addTrendingRank = {
+  dbName: dbName,
+  up: [upQuery],
+  down: [downQuery],
+  dbKind: dbKind
+};
+module.exports = addTrendingRank;
