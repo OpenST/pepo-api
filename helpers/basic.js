7baf54ce29f90e63240d47f1acf3495cd8029c0c
Pankaj Keswani <pankaj@Pankajs-MacBook-Pro.local>
2020-04-06 14:42:04 +0530

    Sanitize username

--- helpers/basic.js
+++ helpers/basic.js
@@ -491,6 +491,16 @@ class BasicHelper {
   }
 
   /**
+   * Sanitize username
+   *
+   * @param username
+   * @returns {string}
+   */
+  sanitizeUsername(username) {
+    return username.toLowerCase().replace(/[^a-z0-9]/g, '');
+  }
+
+  /**
    * Convert date to timestamp in seconds.
    *
    * @param {string} dateStr
