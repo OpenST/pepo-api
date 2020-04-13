97c6a1172a06735fe21b853a5a532c1d8468880d
Gulshan Vasnani <gulshan@ost.com>
2020-04-13 22:18:13 +0530

    Downloading zoom recording files using access token

--- app/services/zoomEvents/meetings/RecordingCompleted.js
+++ app/services/zoomEvents/meetings/RecordingCompleted.js
@@ -39,19 +39,25 @@ class RecordingCompleted extends ZoomEventsForMeetingsBase {
     if (oThis.meetingId) {
       await oThis._setZoomMeetingId();
 
-      await new SaveRecording({ zoomMeetingId: oThis.zoomMeetingId }).perform().catch(async function(e) {
-        const response = responseHelper.error({
-          internal_error_identifier: 's_ze_rc_ap_1',
-          api_error_identifier: 'something_went_wrong',
-          debug_options: {
-            event: oThis.object,
-            errorObject: e
-          }
-        });
+      await new SaveRecording({
+        zoomMeetingId: oThis.zoomMeetingId,
+        recordingFiles: oThis.recordingFiles,
+        accessToken: oThis.accessToken
+      })
+        .perform()
+        .catch(async function(e) {
+          const response = responseHelper.error({
+            internal_error_identifier: 's_ze_rc_ap_1',
+            api_error_identifier: 'something_went_wrong',
+            debug_options: {
+              event: oThis.object,
+              errorObject: e
+            }
+          });
 
-        await createErrorLogsEntry.perform(response, errorLogsConstants.lowSeverity);
-        return Promise.reject(e);
-      });
+          await createErrorLogsEntry.perform(response, errorLogsConstants.lowSeverity);
+          return Promise.reject(e);
+        });
     } else {
       logger.info(`Meeting not found for zoom id ${oThis.zoomMeetingId}`);
     }
